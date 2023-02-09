7f3865ad795e6a45f37fb11f8976a4b3d17a838a
Marion Hauff <marion.hauff@nav.no>
2023-02-09 09:45:17 +0100

    adding scss moduler and stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    rules: {
+        "selector-pseudo-class-no-unknown": [
+          true,
+          {
+            "ignorePseudoClasses": ["global"]
+          }
+        ]
+      }
+}
