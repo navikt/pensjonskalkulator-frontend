4107a45cf52129cba1b62ccb4f79cf61d9c755e0
Marion Hauff <marion.hauff@nav.no>
2023-02-14 08:44:45 +0100

    correcting path to public uncompiled asset

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className={styles.logo} alt="Vite logo" />
+          <img
+            src="/pensjon/kalkulator/vite.svg"
+            className={styles.logo}
+            alt="Vite logo"
+          />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img
