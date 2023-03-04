5ab144734e9f196bcfc856de173d31d946d26f79
Stephen Ramthun <stephenramthun@gmail.com>
2023-03-04 11:08:00 +0100

    convert main to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,14 @@ import { App } from './App/App'
 import { ErrorBoundary } from './ErrorBoundary/ErrorBoundary'
 import '@navikt/ds-css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootId = 'root'
+const root = document.getElementById(rootId)
+
+if (!root) {
+  throw Error(`Missing element with id "${rootId}"`)
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
