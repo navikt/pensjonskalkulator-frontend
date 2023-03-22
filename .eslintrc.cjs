5e83b8c78afd19069dca18e16283d1068c6f51ba
Stephen Ramthun <stephenramthun@gmail.com>
2023-03-22 10:31:16 +0100

    enforce linting pre commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,59 +1,59 @@
 module.exports = {
-    root: true,
-    extends: ['react-typescript', 'prettier'],
-    plugins: ['react', 'import', '@typescript-eslint'],
+  root: true,
+  extends: ['react-typescript', 'prettier'],
+  plugins: ['react', 'import', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            impliedStrict: true,
-        },
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true,
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    env: {
-        browser: true,
-        amd: true,
-        node: true,
-    },
-    rules: {
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        'import/order': [
-            'warn',
-            {
-                pathGroups: [
-                    {
-                        pattern: '{react*,redux*}',
-                        group: 'external',
-                        position: 'before',
-                    },
-                    {
-                        pattern: '~/**',
-                        group: 'external',
-                    },
-                    {
-                        pattern: './*.scss',
-                        group: 'sibling',
-                        position: 'after',
-                    },
-                ],
-                pathGroupsExcludedImportTypes: ['react'],
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true,
-                },
-                'newlines-between': 'always',
-            },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'import/order': [
+      'warn',
+      {
+        pathGroups: [
+          {
+            pattern: '{react*,redux*}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '~/**',
+            group: 'external',
+          },
+          {
+            pattern: './*.scss',
+            group: 'sibling',
+            position: 'after',
+          },
         ],
-    },
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
 }
