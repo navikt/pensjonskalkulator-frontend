01cdafcc093f999a0cdd397fad4365f693508623
Stephen Ramthun <stephenramthun@gmail.com>
2023-03-24 10:00:27 +0100

    use correct port for msw in browser

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -3,7 +3,7 @@ import { rest } from 'msw'
 import pensjonsberegningData from './__mocks__/pensjonsberegning.json' assert { type: 'json' }
 
 export const getHandlers = (
-  host: string = 'http://localhost:8088',
+  host: string = 'http://localhost:5173',
   path: string = `${import.meta.env.BASE_URL}api`
 ) => [
   rest.get(`${host}${path}/pensjonsberegning`, (req, res, ctx) => {
