b8ca0d822f93c07d0a778d5a71a5641104f3012c
Stephen Ramthun <stephenramthun@gmail.com>
2023-03-24 12:24:30 +0100

    use correct api path for msw in browser

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -1,8 +1,9 @@
 import { rest } from 'msw'
+import { PATH } from './paths'
 
 import pensjonsberegningData from './__mocks__/pensjonsberegning.json' assert { type: 'json' }
 
-export const getHandlers = (baseUrl: string = '') => [
+export const getHandlers = (baseUrl: string = PATH) => [
   rest.get(`${baseUrl}/pensjonsberegning`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(pensjonsberegningData), ctx.delay(30))
   }),
--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,4 +1,5 @@
 const HOST = import.meta.env.MODE === 'test' ? 'http://localhost:8088' : ''
-const PATH = `${import.meta.env.BASE_URL}api`
+
+export const PATH = `${import.meta.env.BASE_URL}api`
 
 export const API_BASEURL = `${HOST}${PATH}`
