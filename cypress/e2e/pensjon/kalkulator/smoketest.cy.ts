dbc8e08c337584fdea40cf1bebee9d5cfb70431c
Stephen Ramthun <stephenramthun@gmail.com>
2023-03-24 13:14:14 +0100

    Oppdater smoketest for å passe til ny pensjonsberegningsvisning

--- cypress/e2e/pensjon/kalkulator/smoketest.cy.ts
+++ cypress/e2e/pensjon/kalkulator/smoketest.cy.ts
@@ -8,16 +8,11 @@ describe('Pensjonskalkulator', () => {
     )
     cy.visit('/pensjon/kalkulator')
 
-    cy.contains('Din pensjon')
-    cy.contains(
-      'Hvis du fortsetter å ha samme inntekt som du har i dag kan du tidligst gå av med pensjon ved 67 år hvor du vil få utbetalt 300001 kroner i året'
-    )
-    cy.contains(
-      'Dersom du jobber frem til du er 68 år, vil du få 300002 kroner utbetalt'
-    )
-    cy.contains(
-      'Dersom du jobber frem til du er 69 år, vil du få 300003 kroner utbetalt'
-    )
+    cy.contains('Hei Ola')
+    cy.contains('678 000')
+    cy.contains('300 000')
+    cy.contains('350 000')
+    cy.contains('400 000')
 
     cy.injectAxe()
     cy.checkA11y()
