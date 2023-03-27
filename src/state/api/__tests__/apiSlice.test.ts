75eceb031a2ae8ffcac9660c30cd93e62d175c3c
Marion Hauff <marion.hauff@nav.no>
2023-03-27 15:30:31 +0200

    splitter tester for apiSlice for å isolere mocking

--- src/state/api/__tests__/apiSlice.test.ts
+++ src/state/api/__tests__/apiSlice.test.ts
@@ -1,59 +1,17 @@
-import { vi } from 'vitest'
 import { mockErrorResponse, mockResponse } from '../../../api/server'
-import { API_BASEURL } from '../../../api/paths'
+import { setupStore } from '../../store'
+import { apiSlice } from './../apiSlice'
 
 const pensjonsberegningData = require('../../../api/__mocks__/pensjonsberegning.json')
 
-vi.mock('@reduxjs/toolkit/query/react')
-vi.mock('../apiSlice')
-
+// TODO: fikse bedre typing ved dispatch og Promise result
 describe('apiSlice', () => {
-  const importRealApiSlice = async () => {
-    const apiSliceModuleActual = await vi.importActual('../apiSlice')
-    let apiSliceModule = await import('../apiSlice')
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    apiSliceModule.apiSlice = apiSliceModuleActual.apiSlice
-    return apiSliceModule.apiSlice
-  }
-
-  afterEach(async () => {
-    const realModule: typeof import('@reduxjs/toolkit/query/react') =
-      await vi.importActual('@reduxjs/toolkit/query/react')
-    const reduxUtils: typeof import('@reduxjs/toolkit/query/react') =
-      await import('@reduxjs/toolkit/query/react')
-    reduxUtils.fetchBaseQuery = realModule.fetchBaseQuery
-    reduxUtils.createApi = realModule.createApi
-  })
-
-  it('har riktig baseQuery', async () => {
-    const createAPIMockFunction = vi
-      .fn()
-      .mockReturnValueOnce({ useGetPensjonsberegningQuery: 'lorem' })
-    const fetchBaseQueryMockFunction = vi.fn().mockReturnValueOnce('')
-
-    const reduxUtils = await import('@reduxjs/toolkit/query/react')
-    reduxUtils.fetchBaseQuery = fetchBaseQueryMockFunction
-    reduxUtils.createApi = createAPIMockFunction
-
-    await import('../apiSlice')
-
-    await expect(fetchBaseQueryMockFunction).toHaveBeenCalledWith({
-      baseUrl: API_BASEURL,
-    })
-  })
-
   it('eksponerer riktig endepunkter', async () => {
-    const apiSlice = await importRealApiSlice()
     expect(apiSlice.endpoints).toHaveProperty('getPensjonsberegning')
   })
 
   it('returnerer data ved successfull query', async () => {
-    const apiSlice = await importRealApiSlice()
-
-    const storeModule = await import('../../store')
-    const storeRef = await storeModule.setupStore()
-
+    const storeRef = await setupStore()
     return storeRef
       .dispatch<any>(apiSlice.endpoints.getPensjonsberegning.initiate())
       .then((result: any) => {
@@ -63,12 +21,8 @@ describe('apiSlice', () => {
   })
 
   it('returnerer undefined ved feilende query', async () => {
-    const apiSlice = await importRealApiSlice()
-    const storeModule = await import('../../store')
-    const storeRef = await storeModule.setupStore()
-
+    const storeRef = await setupStore()
     mockErrorResponse('/pensjonsberegning')
-
     return storeRef
       .dispatch<any>(apiSlice.endpoints.getPensjonsberegning.initiate())
       .then((result: any) => {
@@ -78,18 +32,13 @@ describe('apiSlice', () => {
   })
 
   it('kaster feil ved uforventet format på responsen', async () => {
-    const apiSlice = await importRealApiSlice()
-    const storeModule = await import('../../store')
-    const storeRef = await storeModule.setupStore()
-
+    const storeRef = await setupStore()
     const error = console.error
     console.error = () => {}
-
     mockResponse('/pensjonsberegning', {
       status: 200,
       json: [{ 'tullete svar': 'lorem' }],
     })
-
     return storeRef
       .dispatch<any>(apiSlice.endpoints.getPensjonsberegning.initiate())
       .then((result: any) => {
---
+++ src/state/api/__tests__/baseQuery.test.ts
@@ -0,0 +1,24 @@
+import { vi } from 'vitest'
+import { API_BASEURL } from '../../../api/paths'
+
+vi.mock('@reduxjs/toolkit/query/react')
+vi.mock('../apiSlice')
+
+describe('apiSlice', () => {
+  it('har riktig baseQuery', async () => {
+    const createAPIMockFunction = vi
+      .fn()
+      .mockReturnValueOnce({ useGetPensjonsberegningQuery: 'lorem' })
+    const fetchBaseQueryMockFunction = vi.fn().mockReturnValueOnce('')
+
+    const reduxUtils = await import('@reduxjs/toolkit/query/react')
+    reduxUtils.fetchBaseQuery = fetchBaseQueryMockFunction
+    reduxUtils.createApi = createAPIMockFunction
+
+    await import('../apiSlice')
+
+    await expect(fetchBaseQueryMockFunction).toHaveBeenCalledWith({
+      baseUrl: API_BASEURL,
+    })
+  })
+})
