549e1904236829cee28f2f43bdaf66de31c271b0
Marion Hauff <marion.hauff@nav.no>
2023-04-12 10:08:24 +0200

    fjerner check-mark fra aldersknapp

--- src/components/Pensjonssimulering/Pensjonssimulering.module.scss.d.ts
+++ src/components/Pensjonssimulering/Pensjonssimulering.module.scss.d.ts
@@ -3,5 +3,7 @@ declare const classNames: typeof globalClassNames & {
   readonly pensjonssimulering: 'pensjonssimulering'
   readonly chipsWrapper: 'chipsWrapper'
   readonly chipsWrapper__hasGap: 'chipsWrapper__hasGap'
+  readonly 'navds-chips--icon-left': 'navds-chips--icon-left'
+  readonly 'navds-chips__toggle-icon': 'navds-chips__toggle-icon'
 }
 export = classNames
