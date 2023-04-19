2d381aee7c53a48afd0bccf7c9af3f398d44a7e7
Stephen Ramthun <stephenramthun@gmail.com>
2023-04-19 17:47:56 +0200

    Fiks test etter innføring av nytt endepunkt

--- cypress/e2e/pensjon/kalkulator/smoketest.cy.ts
+++ cypress/e2e/pensjon/kalkulator/smoketest.cy.ts
@@ -1,19 +1,13 @@
 describe('Pensjonskalkulator', () => {
   it('rendrer som den skal uten åpenbare a11y-feil', () => {
     cy.intercept(
-      { method: 'GET', url: '/pensjon/kalkulator/api/pensjonsberegning' },
+      { method: 'GET', url: '/pensjon/kalkulator/api/tidligste-uttaksalder' },
       {
-        fixture: 'pensjonsberegning.json',
+        fixture: 'tidligstemuligeuttaksalder.json',
       }
     )
     cy.visit('/pensjon/kalkulator')
-
     cy.contains('Når kan du ta ut alderspensjon?')
-    cy.contains('678 000')
-    cy.contains('300 000')
-    cy.contains('350 000')
-    cy.contains('400 000')
-
     cy.injectAxe()
     cy.checkA11y()
   })
