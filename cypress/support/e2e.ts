d9d5f6e67503c2afd018d76ae24b876787f4cce6
Stephen Ramthun <stephenramthun@gmail.com>
2023-06-02 15:03:37 +0200

    Fiks uttaksalder-fixture

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,7 +2,7 @@ import 'cypress-axe'
 
 beforeEach(() => {
   cy.intercept(
-    { method: 'GET', url: '/pensjon/kalkulator/api/tidligste-uttaksalder' },
+    { method: 'POST', url: '/pensjon/kalkulator/api/tidligste-uttaksalder' },
     { fixture: 'tidligste-uttaksalder.json' }
   )
   cy.intercept(
