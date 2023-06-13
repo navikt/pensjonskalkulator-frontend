091a1a0947781acd84772975830f893d2fa88028
Stephen Ramthun <stephenramthun@gmail.com>
2023-06-13 11:30:28 +0200

    Gjør telemetry-url tilgjengelig for nettleser

--- src/observability/faro.ts
+++ src/observability/faro.ts
@@ -2,7 +2,9 @@ import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
 
 export const initializeLogs = () =>
   initializeFaro({
-    url: import.meta.env.FARO_TELEMETRY_URL ?? 'http://localhost:12347/collect',
+    url:
+      import.meta.env.VITE_FARO_TELEMETRY_URL ??
+      'http://localhost:12347/collect',
     app: {
       name: 'pensjonskalkulator-frontend',
       version: 'dev',
