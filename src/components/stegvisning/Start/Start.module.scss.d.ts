8e75df5a10e1a4bb3e09d085aa8ab082221d54fa
Marion Hauff <marion.hauff@nav.no>
2023-06-15 09:35:57 +0200

    justerer fornavn til brukeren iht formatert string returnert fra backend

--- src/components/stegvisning/Start/Start.module.scss.d.ts
+++ src/components/stegvisning/Start/Start.module.scss.d.ts
@@ -2,7 +2,6 @@ import globalClassNames from '../../../style.d'
 declare const classNames: typeof globalClassNames & {
   readonly section: 'section'
   readonly image: 'image'
-  readonly title: 'title'
   readonly button: 'button'
 }
 export = classNames
