781f926a89cde616e9e989769cbf12bd7aead21d
Marion Hauff <marion.hauff@nav.no>
2023-06-16 12:23:41 +0200

    rettelser i styling på buttonraden i stegvisning

--- src/components/stegvisning/AFP/AFP.module.scss.d.ts
+++ src/components/stegvisning/AFP/AFP.module.scss.d.ts
@@ -1,7 +1,7 @@
 import globalClassNames from '../../../style.d'
 declare const classNames: typeof globalClassNames & {
   readonly section: 'section'
-  readonly ingress: 'ingress'
+  readonly radiogroup: 'radiogroup'
   readonly alert: 'alert'
   readonly button: 'button'
 }
--- src/components/stegvisning/Sivilstand/Sivilstand.module.scss.d.ts
+++ src/components/stegvisning/Sivilstand/Sivilstand.module.scss.d.ts
@@ -2,6 +2,7 @@ import globalClassNames from '../../../style.d'
 declare const classNames: typeof globalClassNames & {
   readonly section: 'section'
   readonly ingress: 'ingress'
+  readonly radiogroup: 'radiogroup'
   readonly button: 'button'
 }
 export = classNames
