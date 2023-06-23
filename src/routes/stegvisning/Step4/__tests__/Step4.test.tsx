77e24c2a9505b4ba70150d27f16dab4b06742f60
Marion Hauff <marion.hauff@nav.no>
2023-06-23 11:23:19 +0200

    refactor test Step4

--- src/routes/stegvisning/Step4/__tests__/Step4.test.tsx
+++ src/routes/stegvisning/Step4/__tests__/Step4.test.tsx
@@ -6,7 +6,7 @@ import { Step4 } from '..'
 import * as Step4Utils from '../utils'
 import { mockResponse, mockErrorResponse } from '@/mocks/server'
 import { RootState } from '@/state/store'
-import { screen, render, waitFor, userEvent } from '@/test-utils'
+import { screen, render, userEvent, waitFor } from '@/test-utils'
 
 describe('Step 4', () => {
   it('redirigerer til Step 2 når brukeren ikke har svart på spørsmålet om samtykke, ', async () => {
@@ -24,24 +24,22 @@ describe('Step 4', () => {
     render(<Step4 />, {
       preloadedState: { userInput: { samtykke: false } } as RootState,
     })
-    await waitFor(() => {
-      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
-        'stegvisning.afp.title'
-      )
-      expect(screen.getAllByRole('radio')).toHaveLength(4)
-    })
+
+    expect(await screen.findByRole('heading', { level: 2 })).toHaveTextContent(
+      'stegvisning.afp.title'
+    )
+    expect(screen.getAllByRole('radio')).toHaveLength(4)
   })
 
   it('rendrer Step 4 slik den skal når brukeren har samtykket, ', async () => {
     render(<Step4 />, {
       preloadedState: { userInput: { samtykke: true } } as RootState,
     })
-    await waitFor(() => {
-      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
-        'stegvisning.afp.title'
-      )
-      expect(screen.getAllByRole('radio')).toHaveLength(4)
-    })
+
+    expect(await screen.findByRole('heading', { level: 2 })).toHaveTextContent(
+      'stegvisning.afp.title'
+    )
+    expect(screen.getAllByRole('radio')).toHaveLength(4)
   })
 
   it('registrerer afp og samboerskap og navigerer videre til beregning når brukeren velger afp og klikker på Neste (gitt at brukeren har samboer)', async () => {
@@ -59,16 +57,15 @@ describe('Step 4', () => {
     const { store } = render(<Step4 />, {
       preloadedState: { userInput: { samtykke: true } } as RootState,
     })
-    await waitFor(async () => {
-      const radioButtons = screen.getAllByRole('radio')
-      await user.click(radioButtons[0])
-      await user.click(screen.getByText('stegvisning.neste'))
-      expect(store.getState().userInput.afp).toBe('ja_offentlig')
-      expect(checkHarSamboerMock).toHaveBeenCalledWith('GIFT')
-      expect(nesteSideMock).toHaveBeenCalledWith(true)
-      expect(navigateMock).toHaveBeenCalledWith('/beregning')
-      expect(store.getState().userInput.samboer).toBe(true)
-    })
+
+    const radioButtons = await screen.findAllByRole('radio')
+    await user.click(radioButtons[0])
+    await user.click(screen.getByText('stegvisning.neste'))
+    expect(store.getState().userInput.afp).toBe('ja_offentlig')
+    expect(checkHarSamboerMock).toHaveBeenCalledWith('GIFT')
+    expect(nesteSideMock).toHaveBeenCalledWith(true)
+    expect(navigateMock).toHaveBeenCalledWith('/beregning')
+    expect(store.getState().userInput.samboer).toBe(true)
   })
 
   it('registrerer afp og navigerer videre til steg 5 når brukeren velger afp og klikker på Neste (gitt at brukeren ikke har samboer)', async () => {
@@ -104,14 +101,13 @@ describe('Step 4', () => {
     const { store } = render(<Step4 />, {
       preloadedState: { userInput: { samtykke: true } } as RootState,
     })
-    await waitFor(async () => {
-      const radioButtons = screen.getAllByRole('radio')
-      await user.click(radioButtons[0])
-      await user.click(screen.getByText('stegvisning.neste'))
-      expect(store.getState().userInput.afp).toBe('ja_offentlig')
-      expect(nesteSideMock).toHaveBeenCalledWith(null)
-      expect(navigateMock).toHaveBeenCalledWith('/sivilstand')
-    })
+
+    const radioButtons = await screen.findAllByRole('radio')
+    await user.click(radioButtons[0])
+    await user.click(screen.getByText('stegvisning.neste'))
+    expect(store.getState().userInput.afp).toBe('ja_offentlig')
+    expect(nesteSideMock).toHaveBeenCalledWith(null)
+    expect(navigateMock).toHaveBeenCalledWith('/sivilstand')
   })
 
   it('sender tilbake til steg 2 når brukeren ikke har tpo-medlemskap og klikker på Tilbake', async () => {
@@ -125,10 +121,9 @@ describe('Step 4', () => {
       () => navigateMock
     )
     render(<Step4 />)
-    await waitFor(async () => {
-      await user.click(screen.getByText('stegvisning.tilbake'))
-      expect(navigateMock).toHaveBeenCalledWith('/samtykke')
-    })
+
+    await user.click(await screen.findByText('stegvisning.tilbake'))
+    expect(navigateMock).toHaveBeenCalledWith('/samtykke')
   })
 
   it('sender tilbake til steg 3 når brukeren har tpo medlemskap og klikker på Tilbake', async () => {
@@ -157,11 +152,10 @@ describe('Step 4', () => {
         userInput: { samtykke: true, afp: 'nei' },
       } as RootState,
     })
-    await waitFor(async () => {
-      await user.click(screen.getByText('stegvisning.avbryt'))
-      expect(navigateMock).toHaveBeenCalledWith('/')
-      expect(store.getState().userInput.samtykke).toBe(null)
-      expect(store.getState().userInput.afp).toBe(null)
-    })
+
+    await user.click(await screen.findByText('stegvisning.avbryt'))
+    expect(navigateMock).toHaveBeenCalledWith('/')
+    expect(store.getState().userInput.samtykke).toBe(null)
+    expect(store.getState().userInput.afp).toBe(null)
   })
 })
