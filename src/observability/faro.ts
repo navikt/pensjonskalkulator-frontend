d9e6fc7db679597019a5ad773b5f496e8b2f6ef0
Stephen Ramthun <stephenramthun@gmail.com>
2023-07-03 10:09:06 +0200

    Håndter telemetry-kall med msw

--- src/observability/faro.ts
+++ src/observability/faro.ts
@@ -3,7 +3,7 @@ import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
 export const initializeLogs = () =>
   initializeFaro({
     url:
-      import.meta.env.VITE_FARO_TELEMETRY_URL ??
+      import.meta.env.VITE_FARO_TELEMETRY_URL ||
       'http://localhost:12347/collect',
     app: {
       name: 'pensjonskalkulator-frontend',
