01fe9232f8dd451b79de89478f4de3effd1314fb
Marion Hauff <marion.hauff@nav.no>
2023-08-03 15:34:44 +0200

    utvider Tabell med visning for desktop

--- src/components/TabellVisning/TabellVisning.tsx
+++ src/components/TabellVisning/TabellVisning.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react'
 
 import { Table } from '@navikt/ds-react'
+import clsx from 'clsx'
 import { SeriesColumnOptions } from 'highcharts'
 
 import { formatAsDecimal } from '@/utils/currency'
 
 import { useTableData } from './hooks'
+
 interface Props {
   series: SeriesColumnOptions[]
   aarArray?: string[]
@@ -20,14 +22,40 @@ export function TabellVisning({ series, aarArray }: Props) {
     <Table className={styles.table}>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell />
+          <Table.HeaderCell className={styles.tableMobileOnly} />
           <Table.HeaderCell scope="col">Alder</Table.HeaderCell>
           <Table.HeaderCell scope="col" className={styles.detailsItemRight}>
-            Sum (kr)
+            Sum<span className={styles.tableMobileOnly}> (kr)</span>
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            scope="col"
+            className={clsx(styles.detailsItemRight, styles.tableDesktopOnly)}
+          >
+            Inntekt
+            <br />
+            (lønn m.m.)
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            scope="col"
+            className={clsx(styles.detailsItemRight, styles.tableDesktopOnly)}
+          >
+            Avtalefestet pensjon (AFP)
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            scope="col"
+            className={clsx(styles.detailsItemRight, styles.tableDesktopOnly)}
+          >
+            Pensjonsavtaler (arbeidsgiver)
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            scope="col"
+            className={clsx(styles.detailsItemRight, styles.tableDesktopOnly)}
+          >
+            Alderspensjon (NAV)
           </Table.HeaderCell>
         </Table.Row>
       </Table.Header>
-      <Table.Body>
+      <Table.Body className={styles.tableMobileOnly}>
         {tableData.map(({ alder, sum, detaljer }, i) => {
           const detaljerGrid = (
             <dl key={i} className={styles.details}>
@@ -41,14 +69,35 @@ export function TabellVisning({ series, aarArray }: Props) {
               ))}
             </dl>
           )
-
           return (
             <Table.ExpandableRow key={i} content={detaljerGrid}>
-              <Table.DataCell scope="row">{alder}</Table.DataCell>
+              <Table.DataCell>{alder}</Table.DataCell>
               <Table.DataCell className={styles.detailsItemRight}>
                 {formatAsDecimal(sum)}
               </Table.DataCell>
             </Table.ExpandableRow>
+          )
+        })}
+      </Table.Body>
+      <Table.Body className={styles.tableDesktopOnly}>
+        {tableData.map(({ alder, sum, detaljer }, i) => {
+          return (
+            <Table.Row key={i}>
+              <Table.DataCell>{alder}</Table.DataCell>
+              <Table.DataCell
+                className={clsx(
+                  styles.detailsItemRight,
+                  styles.detailsItemBold
+                )}
+              >
+                {sum > 0 ? `${formatAsDecimal(sum)} kr` : ''}
+              </Table.DataCell>
+              {detaljer.map(({ subSum }, j) => (
+                <Table.DataCell key={j} className={styles.detailsItemRight}>
+                  {subSum > 0 ? `${formatAsDecimal(subSum)} kr` : ''}
+                </Table.DataCell>
+              ))}
+            </Table.Row>
           )
         })}
       </Table.Body>
