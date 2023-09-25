1ae5c929e769ecc38a93f081124fffcdf7017db7
Bjørn Gilstad <bjorn.gilstad@nav.no>
2023-09-25 10:52:16 +0200

    PEK-60: Oppdater styling

--- src/scss/modules/frame.module.scss.d.ts
+++ src/scss/modules/frame.module.scss.d.ts
@@ -3,7 +3,6 @@ declare const classNames: typeof globalClassNames & {
   readonly frame: 'frame'
   readonly frame__hasPadding: 'frame__hasPadding'
   readonly innerframe: 'innerframe'
-  readonly innerframe__noPadding: 'innerframe__noPadding'
   readonly innerframe__largePadding: 'innerframe__largePadding'
 }
 export = classNames
