e8921736ec482ab3d862e23f1516d103f5687c5a
Marion Hauff <marion.hauff@nav.no>
2023-11-01 15:13:00 +0100

    tvider cypress hovedhistorie med /start

--- cypress/e2e/pensjon/kalkulator/hovedhistorie.cy.ts
+++ cypress/e2e/pensjon/kalkulator/hovedhistorie.cy.ts
@@ -1,6 +1,6 @@
 describe('Hovedhistorie', () => {
   describe('Når jeg som bruker navigerer på nav.no/din pensjon og velger å prøve den nye kalkulatoren,', () => {
-    it('ønsker jeg på landingssiden å få informasjon om hvilke personopplysninger som brukes i kalkulatoren slik at jeg kan ta et valg om jeg ønsker å logge inn.', () => {
+    it('ønsker jeg å få informasjon om ny kalkulator og om jeg er i målgruppen for å bruke den.', () => {
       cy.visit('https://www.nav.no/planlegger-pensjon')
       cy.contains('a', 'Prøv pensjonskalkulatoren')
         .should('have.attr', 'href')
@@ -42,6 +42,12 @@ describe('Hovedhistorie', () => {
       })
     })
     describe('Når jeg vil logge inn for å teste kalkulatoren,', () => {
+      it('ønsker jeg å få informasjon om ny kalkulator og om jeg er i målgruppen for å bruke den.', () => {
+        cy.contains('a', 'Personopplysninger som brukes i enkel kalkulator')
+          .should('have.attr', 'href')
+          .and('include', '/pensjon/kalkulator/personopplysninger')
+      })
+
       it('forventer jeg å kunne logge inn med ID-porten', () => {
         cy.contains('button', 'Logg inn i enkel kalkulator').click()
         cy.location('href').should(
@@ -51,7 +57,38 @@ describe('Hovedhistorie', () => {
       })
     })
   })
-  // describe('Gitt at brukeren er pålogget', () => {})
+  describe('Gitt at brukeren er pålogget', () => {
+    describe('Som bruker som har logget inn på kalkulatoren, ', () => {
+      beforeEach(() => {
+        cy.visit('/pensjon/kalkulator/')
+        cy.wait('@getDecoratorPersonAuth')
+        cy.wait('@getDecoratorLoginAuth')
+        cy.wait('@getAuthSession')
+      })
+      it('forventer jeg å få en startside som ønsker meg velkommen.', () => {
+        cy.contains('button', 'Detaljert kalkulator').should('exist')
+        cy.contains('button', 'Enkel kalkulator').click()
+        cy.contains('Hei Aprikos!')
+      })
+      it('ønsker jeg informasjon om hvilke personopplysninger som brukes i kalkulatoren.', () => {
+        cy.visit('/pensjon/kalkulator/start')
+        cy.contains('a', 'Personopplysninger som brukes i enkel kalkulator')
+          .should('have.attr', 'href')
+          .and('include', '/pensjon/kalkulator/personopplysninger')
+      })
+      it('ønsker jeg å kunne starte kalkulatoren eller avbryte beregningen.', () => {
+        cy.visit('/pensjon/kalkulator/start')
+        cy.contains('button', 'Kom i gang').click()
+        cy.location('href').should(
+          'include',
+          '/pensjon/kalkulator/utenlandsopphold'
+        )
+        cy.go('back')
+        cy.contains('button', 'Avbryt').click()
+        cy.location('href').should('include', '/pensjon/kalkulator/login')
+      })
+    })
+  })
 })
 
 export {}
