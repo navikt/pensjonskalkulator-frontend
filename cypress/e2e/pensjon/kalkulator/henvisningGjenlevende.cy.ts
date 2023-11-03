6a10a8f6057539fcb94609d5204d4f68ccf1ed82
Marion Hauff <marion.hauff@nav.no>
2023-11-03 14:01:15 +0100

    rettelse i formulering av testen

---
+++ cypress/e2e/pensjon/kalkulator/henvisningGjenlevende.cy.ts
@@ -0,0 +1,34 @@
+describe('Henvisning, gjenlevendepensjon ', () => {
+  describe('Når jeg som bruker som har gjenlevendepensjon logger inn,', () => {
+    beforeEach(() => {
+      cy.intercept(
+        { method: 'GET', url: '/pensjon/kalkulator/api/sak-status' },
+        {
+          harUfoeretrygdEllerGjenlevendeytelse: true,
+        }
+      ).as('getSakStatus')
+    })
+    it('Forventer jeg informasjon om at jeg ikke kan bruke enkel kalkulator. Jeg ønsker å kunne gå til detaljert kalkulator eller avbryte.', () => {
+      cy.login()
+      cy.wait('@getDecoratorPersonAuth')
+      cy.wait('@getDecoratorLoginAuth')
+      cy.wait('@getAuthSession')
+      cy.contains('Kom i gang').should('not.exist')
+      cy.contains('Du kan dessverre ikke bruke enkel kalkulator').should(
+        'exist'
+      )
+      cy.contains('button', 'Detaljert kalkulator').click()
+      cy.origin('https://idporten.difi.no', () => {
+        cy.get('h1').contains('Velg elektronisk ID')
+      })
+      cy.visit('/pensjon/kalkulator/start')
+      cy.contains('button', 'Avbryt').click()
+      cy.location('href').should(
+        'eq',
+        'http://localhost:4173/pensjon/kalkulator/login'
+      )
+    })
+  })
+})
+
+export {}
