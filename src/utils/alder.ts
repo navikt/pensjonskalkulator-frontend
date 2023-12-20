61fb075580a6ae15bed31ad2fe4963cbf46c7e34
Marion Hauff <marion.hauff@nav.no>
2023-12-20 11:55:43 +0100

    forenkler alder utils functions

--- src/utils/alder.ts
+++ src/utils/alder.ts
@@ -1,7 +1,7 @@
-import { lastDayOfYear, isBefore, isSameDay } from 'date-fns'
+import { isBefore, isSameDay } from 'date-fns'
 
 export const isFoedtFoer1963 = (foedselsdato: string): boolean => {
-  const LAST_DAY_1962 = lastDayOfYear(new Date(1962, 1, 1))
+  const LAST_DAY_1962 = new Date(1962, 11, 31)
   return (
     isBefore(new Date(foedselsdato), LAST_DAY_1962) ||
     isSameDay(new Date(foedselsdato), LAST_DAY_1962)
@@ -9,7 +9,7 @@ export const isFoedtFoer1963 = (foedselsdato: string): boolean => {
 }
 
 export const isFoedtFoer1964 = (foedselsdato: string): boolean => {
-  const LAST_DAY_1963 = lastDayOfYear(new Date(1963, 1, 1))
+  const LAST_DAY_1963 = new Date(1963, 11, 31)
   return (
     isBefore(new Date(foedselsdato), LAST_DAY_1963) ||
     isSameDay(new Date(foedselsdato), LAST_DAY_1963)
