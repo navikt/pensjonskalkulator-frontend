a61082ee57ea1be695e6f3359a41b5f3ecc7333c
Marion Hauff <marion.hauff@nav.no>
2024-01-02 12:39:34 +0100

    reaktiverer utkommenterte tester

--- src/components/EndreInntekt/__tests__/EndreInntekt.test.tsx
+++ src/components/EndreInntekt/__tests__/EndreInntekt.test.tsx
@@ -107,89 +107,93 @@ describe('EndreInntekt', () => {
       ).not.toBeInTheDocument()
     })
 
-    // it('brukeren kan gå ut av modulen og la inntekt uendret', async () => {
-    //   const user = userEvent.setup()
+    it('brukeren kan gå ut av modulen og la inntekt uendret', async () => {
+      const user = userEvent.setup()
 
-    //   const fakeInntektApiCall = {
-    //     queries: {
-    //       ['getInntekt(undefined)']: {
-    //         status: 'fulfilled',
-    //         endpointName: 'getInntekt',
-    //         requestId: 'xTaE6mOydr5ZI75UXq4Wi',
-    //         startedTimeStamp: 1688046411971,
-    //         data: {
-    //           beloep: 521338,
-    //           aar: 2021,
-    //         },
-    //         fulfilledTimeStamp: 1688046412103,
-    //       },
-    //     },
-    //   }
-    //   const { store } = render(<EndreInntekt />, {
-    //     preloadedState: {
-    //       /* eslint-disable @typescript-eslint/ban-ts-comment */
-    //       // @ts-ignore
-    //       api: { ...fakeInntektApiCall },
-    //       userInput: { ...userInputInitialState, samtykke: false },
-    //     },
-    //   })
-    //   expect(selectAarligInntektFoerUttak(store.getState())).toBe(521338)
-    //   expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
-    //     null
-    //   )
+      const fakeInntektApiCall = {
+        queries: {
+          ['getInntekt(undefined)']: {
+            status: 'fulfilled',
+            endpointName: 'getInntekt',
+            requestId: 'xTaE6mOydr5ZI75UXq4Wi',
+            startedTimeStamp: 1688046411971,
+            data: {
+              beloep: 521338,
+              aar: 2021,
+            },
+            fulfilledTimeStamp: 1688046412103,
+          },
+        },
+      }
+      const { store } = render(<EndreInntekt />, {
+        preloadedState: {
+          /* eslint-disable @typescript-eslint/ban-ts-comment */
+          // @ts-ignore
+          api: { ...fakeInntektApiCall },
+          userInput: { ...userInputInitialState, samtykke: false },
+        },
+      })
+      expect(selectAarligInntektFoerUttak(store.getState())).toBe(521338)
+      expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
+        null
+      )
 
-    //   await user.click(screen.getByText('inntekt.endre_inntekt_modal.open.button'))
-    //   await user.click(screen.getByText('stegvisning.avbryt'))
-    //   expect(selectAarligInntektFoerUttak(store.getState())).toBe(521338)
-    //   expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
-    //     null
-    //   )
-    // })
+      await user.click(
+        screen.getByText('inntekt.endre_inntekt_modal.open.button')
+      )
+      await user.click(screen.getByText('stegvisning.avbryt'))
+      expect(selectAarligInntektFoerUttak(store.getState())).toBe(521338)
+      expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
+        null
+      )
+    })
   })
 
-  // describe('Gitt at brukeren ikke har noe inntekt', () => {
-  //   it('viser riktig tittel og tekst med 0 inntekt, og brukeren kan overskrive den', async () => {
-  //     const fakeInntektApiCall = {
-  //       queries: {
-  //         ['getInntekt(undefined)']: {
-  //           status: 'fulfilled',
-  //           endpointName: 'getInntekt',
-  //           requestId: 'xTaE6mOydr5ZI75UXq4Wi',
-  //           startedTimeStamp: 1688046411971,
-  //           data: {
-  //             beloep: 0,
-  //             aar: 2021,
-  //           },
-  //           fulfilledTimeStamp: 1688046412103,
-  //         },
-  //       },
-  //     }
-  //     const user = userEvent.setup()
-  //     const { store } = render(<EndreInntekt />, {
-  //       preloadedState: {
-  //         /* eslint-disable @typescript-eslint/ban-ts-comment */
-  //         // @ts-ignore
-  //         api: { ...fakeInntektApiCall },
-  //         userInput: { ...userInputInitialState, samtykke: false },
-  //       },
-  //     })
+  describe('Gitt at brukeren ikke har noe inntekt', () => {
+    it('viser riktig tittel og tekst med 0 inntekt, og brukeren kan overskrive den', async () => {
+      const fakeInntektApiCall = {
+        queries: {
+          ['getInntekt(undefined)']: {
+            status: 'fulfilled',
+            endpointName: 'getInntekt',
+            requestId: 'xTaE6mOydr5ZI75UXq4Wi',
+            startedTimeStamp: 1688046411971,
+            data: {
+              beloep: 0,
+              aar: 2021,
+            },
+            fulfilledTimeStamp: 1688046412103,
+          },
+        },
+      }
+      const user = userEvent.setup()
+      const { store } = render(<EndreInntekt />, {
+        preloadedState: {
+          /* eslint-disable @typescript-eslint/ban-ts-comment */
+          // @ts-ignore
+          api: { ...fakeInntektApiCall },
+          userInput: { ...userInputInitialState, samtykke: false },
+        },
+      })
 
-  //     expect(selectAarligInntektFoerUttak(store.getState())).toBe(0)
-  //     expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
-  //       null
-  //     )
+      expect(selectAarligInntektFoerUttak(store.getState())).toBe(0)
+      expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
+        null
+      )
 
-  //     await user.click(screen.getByText('inntekt.endre_inntekt_modal.open.button'))
-  //     await user.type(screen.getByTestId('inntekt-textfield'), '123000')
+      await user.click(
+        screen.getByText('inntekt.endre_inntekt_modal.open.button')
+      )
+      await user.type(screen.getByTestId('inntekt-textfield'), '123000')
 
-  //     await user.click(screen.getByText('inntekt.endre_inntekt_modal.button'))
-  //     expect(
-  //       selectAarligInntektFoerUttakFraSkatt(store.getState())?.beloep
-  //     ).toBe(0)
-  //     expect(selectAarligInntektFoerUttak(store.getState())).toBe(123000)
-  //     expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
-  //       123000
-  //     )
-  //   })
-  // })
+      await user.click(screen.getByText('inntekt.endre_inntekt_modal.button'))
+      expect(
+        selectAarligInntektFoerUttakFraSkatt(store.getState())?.beloep
+      ).toBe(0)
+      expect(selectAarligInntektFoerUttak(store.getState())).toBe(123000)
+      expect(selectAarligInntektFoerUttakFraBrukerInput(store.getState())).toBe(
+        123000
+      )
+    })
+  })
 })
