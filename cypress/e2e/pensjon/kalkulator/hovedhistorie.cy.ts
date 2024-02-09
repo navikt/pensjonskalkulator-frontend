4ff00afc254f28b043e93c5c38b12c4d8498fd4f
Marion Hauff <marion.hauff@nav.no>
2024-02-09 13:12:32 +0100

    fjerner cypress historier som dekker avbryt knapp i footer

--- cypress/e2e/pensjon/kalkulator/hovedhistorie.cy.ts
+++ cypress/e2e/pensjon/kalkulator/hovedhistorie.cy.ts
@@ -380,13 +380,10 @@ describe('Hovedhistorie', () => {
           .and('include', 'https://www.intern.dev.nav.no/pselv/simulering.jsf')
       })
 
-      it('ønsker jeg å kunne starte ny beregning, eller avbryte beregningen.', () => {
+      it('ønsker jeg å kunne starte ny beregning.', () => {
         cy.contains('button', '62 år og 10 md.').click()
         cy.contains('button', 'Tilbake til start').click({ force: true })
         cy.location('href').should('include', '/pensjon/kalkulator/start')
-        cy.fillOutStegvisning({ samtykke: false })
-        cy.contains('button', 'Avbryt').click({ force: true })
-        cy.location('href').should('include', '/pensjon/kalkulator/login')
       })
     })
 
