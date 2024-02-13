c126b53716f9f05800d4a7b87e5345f4b19c9d55
Marion Hauff <marion.hauff@nav.no>
2024-02-13 12:10:26 +0100

    design justeringer

--- src/components/LightBlueFooter/LightBlueFooter.module.scss.d.ts
+++ src/components/LightBlueFooter/LightBlueFooter.module.scss.d.ts
@@ -1,7 +1,7 @@
 import globalClassNames from "../../style.d";
 declare const classNames: typeof globalClassNames & {
   readonly section: "section";
-  readonly innerwrapper: "innerwrapper";
   readonly button: "button";
+  readonly innerwrapper: "innerwrapper";
 };
 export = classNames;
