f4fad8909885470d0dd85b5fc0f70b0a91752a08
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-02-20 15:12:56 +0100

    PEK-298: Fix useIsMobile test to cover resize

--- src/utils/__tests__/useIsMobile.test.tsx
+++ src/utils/__tests__/useIsMobile.test.tsx
@@ -1,19 +1,30 @@
-import { renderHook } from '@testing-library/react'
+import { fireEvent, renderHook } from '@testing-library/react'
 
 import { useIsMobile } from '../useIsMobile'
 
 describe('useIsMobile', () => {
   it('is true with 500 px', () => {
-    global.innerWidth = 500
+    window.innerWidth = 500
     const { result } = renderHook(() => useIsMobile())
 
     expect(result.current).toBe(true)
   })
 
   it('is false with 800 px', () => {
-    global.innerWidth = 800
+    window.innerWidth = 800
     const { result } = renderHook(() => useIsMobile())
 
     expect(result.current).toBe(false)
+  })
+
+  it('change on resize', async () => {
+    window.innerWidth = 800
+    const { result } = renderHook(() => useIsMobile())
+    expect(result.current).toBe(false)
+
+    window.innerWidth = 500
+    fireEvent(window, new Event('resize'))
+
+    expect(result.current).toBe(true)
   })
 })
