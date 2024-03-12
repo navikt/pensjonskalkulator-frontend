0329f3e2285b2acdb8057f1545a155e67694d01f
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-03-12 13:23:57 +0100

    PEK-298: Legg til margin over avtalegruppe Header

--- src/components/Pensjonsavtaler/PensjonsavtalerTable.module.scss.d.ts
+++ src/components/Pensjonsavtaler/PensjonsavtalerTable.module.scss.d.ts
@@ -1,5 +1,6 @@
 import globalClassNames from "../../style.d";
 declare const classNames: typeof globalClassNames & {
+  readonly TableHeader: "TableHeader";
   readonly TableRader__noBottomBorder: "TableRader__noBottomBorder";
   readonly TableRader__alignTop: "TableRader__alignTop";
 };
