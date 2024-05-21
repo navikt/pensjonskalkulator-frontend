1c10b7dfa55a1da6f288deb15649cb67f0f54449
Marion Hauff <marion.hauff@nav.no>
2024-05-21 10:44:01 +0200

    implementere steg 5 og 6

--- cypress/e2e/pensjon/kalkulator/ufoeretrygd.cy.ts
+++ cypress/e2e/pensjon/kalkulator/ufoeretrygd.cy.ts
@@ -100,8 +100,8 @@ describe('Ufoeretrygd', () => {
     })
   })
 
-  describe.skip('Som bruker som har logget inn på kalkulatoren, som mottar uføretrygd og som ikke har rett til AFP', () => {
-    describe('Når jeg er kommet til beregningssiden', () => {
+  describe('Som bruker som har logget inn på kalkulatoren, som mottar uføretrygd og som ikke har rett til AFP', () => {
+    describe('Når jeg har 100 % uføretrygd og er kommet til beregningssiden', () => {
       beforeEach(() => {
         cy.intercept(
           {
@@ -129,14 +129,85 @@ describe('Ufoeretrygd', () => {
         cy.contains('button', 'Neste').click()
         cy.get('[type="radio"]').eq(2).check()
         cy.contains('button', 'Neste').click()
-        cy.get('[type="radio"]').eq(0).check()
-        cy.contains('button', 'Neste').click()
-
-        cy.wait('@fetchTidligsteUttaksalder')
+        cy.get('[type="radio"]').eq(1).check()
+        cy.contains('button', 'Beregn pensjon').click()
       })
 
       it('forventer jeg informasjon om hvilken grad uføretrygd jeg har.', () => {
-        cy.contains('Pensjonsgivende inntekt frem til pensjon').should('exist')
+        cy.contains('Du har 100 % uføretrygd.').should('exist')
+      })
+
+      it('forventer jeg tilpasset informasjon i read more «om pensjonsalder og uføretrygd»', () => {
+        cy.contains('Om pensjonsalder og uføretrygd').click()
+        cy.contains(
+          '100 % uføretrygd kan ikke kombineres med alderspensjon. Det er derfor ikke mulig å beregne alderspensjon før 67 år i kalkulatoren. Ved 67 år går 100 % uføretrygd automatisk over til 100 % alderspensjon.'
+        ).should('exist')
+      })
+
+      it('forventer jeg å kunne velge alder fra 67 år til 75 år.', () => {
+        cy.contains('Når vil du ta ut alderspensjon?').should('exist')
+        const agesInRange = [67, 68, 69, 70, 71, 72, 73, 74, 75]
+        agesInRange.forEach((age) => {
+          cy.contains('button', `${age} år`).should('exist')
+        })
+        cy.contains('button', `66 år`).should('not.exist')
+        cy.contains('button', `76 år`).should('not.exist')
+      })
+    })
+
+    describe('Når jeg har gradert uføretrygd og er kommet til beregningssiden', () => {
+      beforeEach(() => {
+        cy.intercept(
+          {
+            method: 'GET',
+            url: '/pensjon/kalkulator/api/feature/pensjonskalkulator.enable-ufoere',
+          },
+          {
+            enabled: true,
+          }
+        ).as('getFeatureToggleUfoere')
+        cy.intercept(
+          { method: 'GET', url: '/pensjon/kalkulator/api/v1/ufoeregrad' },
+          {
+            ufoeregrad: 75,
+          }
+        ).as('getUfoeregrad')
+
+        cy.login()
+        cy.contains('button', 'Kom i gang').click()
+        cy.get('[type="radio"]').last().check()
+        cy.contains('button', 'Neste').click()
+        cy.get('[type="radio"]').first().check()
+        cy.contains('button', 'Neste').click()
+        cy.wait('@getTpoMedlemskap')
+        cy.contains('button', 'Neste').click()
+        cy.get('[type="radio"]').eq(2).check()
+        cy.contains('button', 'Neste').click()
+        cy.get('[type="radio"]').eq(1).check()
+        cy.contains('button', 'Beregn pensjon').click()
+      })
+
+      it('forventer jeg informasjon om hvilken grad uføretrygd jeg har.', () => {
+        cy.contains(
+          'Du har 75 % uføretrygd. Her kan du beregne 100 % alderspensjon fra 67 år.'
+        ).should('exist')
+      })
+
+      it('forventer jeg tilpasset informasjon i read more «om pensjonsalder og uføretrygd»', () => {
+        cy.contains('Om pensjonsalder og uføretrygd').click()
+        cy.contains(
+          'Det er mulig å kombinere gradert uføretrygd og gradert alderspensjon fra 62 år, så lenge du har høy nok opptjening til å ta ut alderspensjon. Graden av uføretrygd og alderspensjon kan ikke overstige 100 %.'
+        ).should('exist')
+      })
+
+      it('forventer jeg å kunne velge alder fra 67 år til 75 år.', () => {
+        cy.contains('Når vil du ta ut alderspensjon?').should('exist')
+        const agesInRange = [67, 68, 69, 70, 71, 72, 73, 74, 75]
+        agesInRange.forEach((age) => {
+          cy.contains('button', `${age} år`).should('exist')
+        })
+        cy.contains('button', `66 år`).should('not.exist')
+        cy.contains('button', `76 år`).should('not.exist')
       })
     })
   })
