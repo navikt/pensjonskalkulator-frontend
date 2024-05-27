e62bd724084f873ac15ed8bf6637529ef5c816d2
Marion Hauff <marion.hauff@nav.no>
2024-05-27 13:25:57 +0200

    oppdaterer nais.js mock slik at app name som kreves av faro alltid er med

--- public/src/nais.js
+++ public/src/nais.js
@@ -1,4 +1,8 @@
 // Dette er en dummy nais.js slik at cypress ikke feiler ved å fetche den (unngår 404).
 export default {
   telemetryCollectorURL: '#',
+  app: {
+    name: 'pensjonskalkulator-frontend',
+    version: 'dev',
+  },
 }
