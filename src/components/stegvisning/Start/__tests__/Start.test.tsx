2309d8ee48d03c5e578face91f006d4afd408ed1
Marion Hauff <marion.hauff@nav.no>
2024-09-19 14:40:03 +0200

    legger til tester for Start steget med loependeVedtak

--- src/components/stegvisning/Start/__tests__/Start.test.tsx
+++ src/components/stegvisning/Start/__tests__/Start.test.tsx
@@ -9,18 +9,20 @@ describe('stegvisning - Start', () => {
   const onCancelMock = vi.fn()
   const onNextMock = vi.fn()
 
-  it('rendrer slik den skal med navn, med riktig heading, bilde, tekst og knapper', async () => {
-    const result = render(
-      <Start navn="Ola" onCancel={onCancelMock} onNext={onNextMock} />
-    )
-
-    await waitFor(() => {
-      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
-        'stegvisning.start.title Ola!'
-      )
-      expect(result.asFragment()).toMatchSnapshot()
-    })
-  })
+  const loependeVedtak = {
+    alderspensjon: {
+      grad: 0,
+    },
+    ufoeretrygd: {
+      grad: 0,
+    },
+    afpPrivat: {
+      grad: 0,
+    },
+    afpOffentlig: {
+      grad: 0,
+    },
+  }
 
   it('kaller navigate når shouldRedirectTo er angitt', async () => {
     const navigateMock = vi.fn()
@@ -33,6 +35,7 @@ describe('stegvisning - Start', () => {
       <Start
         shouldRedirectTo={randomPath}
         navn=""
+        loependeVedtak={loependeVedtak}
         onCancel={onCancelMock}
         onNext={onNextMock}
       />
@@ -40,9 +43,32 @@ describe('stegvisning - Start', () => {
     expect(navigateMock).toHaveBeenCalledWith(randomPath)
   })
 
+  it('rendrer slik den skal med navn, med riktig heading, bilde, tekst og knapper', async () => {
+    const result = render(
+      <Start
+        navn="Ola"
+        loependeVedtak={loependeVedtak}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        'stegvisning.start.title Ola!'
+      )
+      expect(result.asFragment()).toMatchSnapshot()
+    })
+  })
+
   it('rendrer slik den skal uten navn, med riktig heading, bilde, tekst og knapper', async () => {
     const result = render(
-      <Start navn="" onCancel={onCancelMock} onNext={onNextMock} />
+      <Start
+        navn=""
+        loependeVedtak={loependeVedtak}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
     )
 
     await waitFor(() => {
@@ -53,26 +79,203 @@ describe('stegvisning - Start', () => {
     })
   })
 
+  it('rendrer slik den skal med vedtak om alderspensjon', async () => {
+    const result = render(
+      <Start
+        navn="Ola"
+        loependeVedtak={{
+          alderspensjon: {
+            grad: 50,
+          },
+          ufoeretrygd: {
+            grad: 0,
+          },
+          afpPrivat: {
+            grad: 0,
+          },
+          afpOffentlig: {
+            grad: 0,
+          },
+        }}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        'stegvisning.start.title Ola!'
+      )
+      expect(screen.getByText('Du har nå', { exact: false })).toBeVisible()
+      expect(
+        screen.getByText('50 % alderspensjon', { exact: false })
+      ).toBeVisible()
+      expect(result.asFragment()).toMatchSnapshot()
+    })
+  })
+
+  it('rendrer slik den skal med vedtak om alderspensjon og uføregrad', async () => {
+    const result = render(
+      <Start
+        navn="Ola"
+        loependeVedtak={{
+          alderspensjon: {
+            grad: 50,
+          },
+          ufoeretrygd: {
+            grad: 80,
+          },
+          afpPrivat: {
+            grad: 0,
+          },
+          afpOffentlig: {
+            grad: 0,
+          },
+        }}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        'stegvisning.start.title Ola!'
+      )
+      expect(screen.getByText('Du har nå', { exact: false })).toBeVisible()
+      expect(
+        screen.getByText('50 % alderspensjon', { exact: false })
+      ).toBeVisible()
+      expect(
+        screen.getByText('80 % uføretrygd', { exact: false })
+      ).toBeVisible()
+      expect(result.asFragment()).toMatchSnapshot()
+    })
+  })
+
+  it('rendrer slik den skal med vedtak om alderspensjon og AFP privat', async () => {
+    const result = render(
+      <Start
+        navn="Ola"
+        loependeVedtak={{
+          alderspensjon: {
+            grad: 50,
+          },
+          ufoeretrygd: {
+            grad: 0,
+          },
+          afpPrivat: {
+            grad: 100,
+          },
+          afpOffentlig: {
+            grad: 0,
+          },
+        }}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        'stegvisning.start.title Ola!'
+      )
+      expect(screen.getByText('Du har nå', { exact: false })).toBeVisible()
+      expect(
+        screen.getByText('50 % alderspensjon', { exact: false })
+      ).toBeVisible()
+      expect(
+        screen.getByText('AFP i privat sektor', { exact: false })
+      ).toBeVisible()
+      expect(result.asFragment()).toMatchSnapshot()
+    })
+  })
+
+  it('rendrer slik den skal med vedtak om alderspensjon og AFP offentlig', async () => {
+    const result = render(
+      <Start
+        navn="Ola"
+        loependeVedtak={{
+          alderspensjon: {
+            grad: 50,
+          },
+          ufoeretrygd: {
+            grad: 0,
+          },
+          afpPrivat: {
+            grad: 0,
+          },
+          afpOffentlig: {
+            grad: 100,
+          },
+        }}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        'stegvisning.start.title Ola!'
+      )
+      expect(screen.getByText('Du har nå', { exact: false })).toBeVisible()
+      expect(
+        screen.getByText('50 % alderspensjon', { exact: false })
+      ).toBeVisible()
+      expect(
+        screen.getByText('AFP i offentlig sektor', { exact: false })
+      ).toBeVisible()
+      expect(result.asFragment()).toMatchSnapshot()
+    })
+  })
+
   it('kaller onNext når brukeren klikker på Neste', async () => {
     const user = userEvent.setup()
-    render(<Start navn="Ola" onCancel={onCancelMock} onNext={onNextMock} />)
+    render(
+      <Start
+        navn="Ola"
+        loependeVedtak={loependeVedtak}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
     await user.click(screen.getByText('stegvisning.start.button'))
     expect(onNextMock).toHaveBeenCalled()
   })
 
   it('viser ikke avbryt knapp når onCancel ikke er definert', async () => {
-    render(<Start navn="Ola" onCancel={undefined} onNext={onNextMock} />)
+    render(
+      <Start
+        navn="Ola"
+        loependeVedtak={loependeVedtak}
+        onCancel={undefined}
+        onNext={onNextMock}
+      />
+    )
     expect(screen.queryByText('stegvisning.avbryt')).not.toBeInTheDocument()
   })
 
   it('viser ikke avbryt knapp når onCancel ikke er definert', async () => {
-    render(<Start navn="Ola" onCancel={undefined} onNext={onNextMock} />)
+    render(
+      <Start
+        navn="Ola"
+        loependeVedtak={loependeVedtak}
+        onCancel={undefined}
+        onNext={onNextMock}
+      />
+    )
     expect(screen.queryByText('stegvisning.avbryt')).not.toBeInTheDocument()
   })
 
   it('kaller onCancel når brukeren klikker på Avbryt', async () => {
     const user = userEvent.setup()
-    render(<Start navn="Ola" onCancel={onCancelMock} onNext={onNextMock} />)
+    render(
+      <Start
+        navn="Ola"
+        loependeVedtak={loependeVedtak}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
     expect(screen.getByText('stegvisning.avbryt')).toBeInTheDocument()
     await user.click(screen.getByText('stegvisning.avbryt'))
     expect(onCancelMock).toHaveBeenCalled()
