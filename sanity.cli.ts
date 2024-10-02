7750d21300354527e603291751b8b760a332dc1e
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-10-02 14:49:58 +0200

    PEK-609: Add deploy av sanity cms
    Co-authored-by: Marion Hauff <marion.hauff@nav.no>

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,17 +5,6 @@ export default defineCliConfig({
     projectId: 'g2by7q6m',
     dataset: 'production',
   },
-  vite: {
-    build: {
-      outDir: 'dist-sanity',
-      emptyOutDir: true,
-    },
-    resolve: {
-      alias: {
-        '@': __dirname,
-      },
-    },
-  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'sanity'
+import { createAuthStore, defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
@@ -15,4 +15,17 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+  auth: createAuthStore({
+    projectId: 'g2by7q6m',
+    dataset: 'production',
+    mode: 'append',
+    redirectOnSingle: true,
+    providers: [
+      {
+        name: 'saml',
+        title: 'NAV SSO',
+        url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/f3270b37',
+      },
+    ],
+  }),
 })
