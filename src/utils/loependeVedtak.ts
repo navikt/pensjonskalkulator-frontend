5de204e617b9e9799d2f559025cf1a36b8544e54
Marion Hauff <marion.hauff@nav.no>
2024-10-28 14:28:30 +0100

    korrigerer lint feil

---
+++ src/utils/loependeVedtak.ts
@@ -0,0 +1,7 @@
+// TODO PEK-693 skrive tester
+export const isLoependeVedtakEndring = (loependeVedtak: LoependeVedtak) => {
+  const { alderspensjon, ufoeretrygd } = loependeVedtak
+  const har0AlderspensjonOg100Ufoeretrygd =
+    alderspensjon?.grad === 0 && ufoeretrygd.grad === 100
+  return !!alderspensjon && !har0AlderspensjonOg100Ufoeretrygd
+}
