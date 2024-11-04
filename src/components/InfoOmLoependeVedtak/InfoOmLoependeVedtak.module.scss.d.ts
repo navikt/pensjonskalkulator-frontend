8720c8090c84ad8ff3f6146f7c4acc3802ed6a38
Marion Hauff <marion.hauff@nav.no>
2024-11-04 14:53:50 +0100

    legger til informasjon om loepende bedtak på toppen av beregningssiden

---
+++ src/components/InfoOmLoependeVedtak/InfoOmLoependeVedtak.module.scss.d.ts
@@ -0,0 +1,6 @@
+import globalClassNames from "../../style.d";
+declare const classNames: typeof globalClassNames & {
+  readonly container: "container";
+  readonly container__hasMobilePadding: "container__hasMobilePadding";
+};
+export = classNames;
---
+++ src/components/Simulering/SimuleringPensjonsavtalerAlert/SimuleringPensjonsavtalerAlert.module.scss.d.ts
@@ -0,0 +1,8 @@
+import globalClassNames from "../../../style.d";
+declare const classNames: typeof globalClassNames & {
+  readonly alert: "alert";
+  readonly info: "info";
+  readonly infoIcon: "infoIcon";
+  readonly infoText: "infoText";
+};
+export = classNames;
