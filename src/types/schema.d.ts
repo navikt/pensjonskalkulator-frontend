383bc3d6df237bb6d8d848d41c6544c806852cf9
Marion Hauff <marion.hauff@nav.no>
2024-11-04 10:54:03 +0100

    oppdaterer backend types

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -696,6 +696,8 @@ export interface components {
         | 'ALDERSPENSJON'
         | 'ALDERSPENSJON_MED_AFP_PRIVAT'
         | 'ALDERSPENSJON_MED_AFP_OFFENTLIG_LIVSVARIG'
+        | 'ENDRING_ALDERSPENSJON'
+        | 'ENDRING_ALDERSPENSJON_MED_AFP_PRIVAT'
       /** @enum {string} */
       sivilstand?:
         | 'UNKNOWN'
@@ -794,7 +796,7 @@ export interface components {
       grad: number
       /** Format: date */
       fom: string
-      sisteUtbetaling?: components['schemas']['UtbetalingSisteMaanedV2']
+      sisteUtbetaling?: components['schemas']['UtbetalingV2']
     }
     LoependeFraV2: {
       /** Format: date */
@@ -811,8 +813,7 @@ export interface components {
       /** Format: int32 */
       grad: number
     }
-    UtbetalingSisteMaanedV2: {
-      /** Format: int32 */
+    UtbetalingV2: {
       beloep: number
       /** Format: date */
       utbetalingsdato: string
