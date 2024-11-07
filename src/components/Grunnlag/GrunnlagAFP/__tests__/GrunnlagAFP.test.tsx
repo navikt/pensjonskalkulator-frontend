b06280d55053023160a4aabaf8a955ca1e53972a
Marion Hauff <marion.hauff@nav.no>
2024-11-07 15:09:19 +0100

    legger til regekl for å skjule AFP fra grunnlag for brukere med UT eldre enn 62

--- src/components/Grunnlag/GrunnlagAFP/__tests__/GrunnlagAFP.test.tsx
+++ src/components/Grunnlag/GrunnlagAFP/__tests__/GrunnlagAFP.test.tsx
@@ -1,5 +1,7 @@
 import * as ReactRouterUtils from 'react-router'
 
+import { add, endOfDay, format } from 'date-fns'
+
 import { GrunnlagAFP } from '..'
 import {
   fulfilledGetLoependeVedtak0Ufoeregrad,
@@ -10,6 +12,7 @@ import {
 } from '@/mocks/mockedRTKQueryApiCalls'
 import { userInputInitialState } from '@/state/userInput/userInputReducer'
 import { render, screen, userEvent } from '@/test-utils'
+import { DATE_BACKEND_FORMAT } from '@/utils/dates'
 
 describe('Grunnlag - AFP', () => {
   it('Når brukeren har valgt AFP offentlig og samtykket til beregning av den, viser riktig tittel med formatert inntekt og tekst', async () => {
@@ -165,95 +168,160 @@ describe('Grunnlag - AFP', () => {
   })
 
   describe('Gitt at brukeren har uføretrygd,', () => {
-    it('Når hen har valgt AFP offentlig, viser riktig tittel med formatert inntekt og tekst', async () => {
-      render(<GrunnlagAFP goToStart={vi.fn()} />, {
-        preloadedState: {
-          api: {
-            // @ts-ignore
-            queries: { ...fulfilledGetLoependeVedtak75Ufoeregrad },
-          },
-          userInput: {
-            ...userInputInitialState,
-            afp: 'ja_offentlig',
-          },
-        },
+    describe('Gitt at brukeren er eldre enn minimum uttaksalderen,', () => {
+      const minAlderYearsBeforeNow = add(endOfDay(new Date()), {
+        years: -63,
+        months: -1,
       })
+      const foedselsdato = format(minAlderYearsBeforeNow, DATE_BACKEND_FORMAT)
 
-      expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
-      expect(
-        screen.getByText('afp.offentlig (grunnlag.afp.ikke_beregnet)')
-      ).toBeVisible()
-      expect(
-        screen.getByText(
-          'Når du mottar uføretrygd, kan du ikke beregne AFP i kalkulatoren.',
-          { exact: false }
-        )
-      ).toBeInTheDocument()
+      const mockedQueries = {
+        ...fulfilledGetLoependeVedtak75Ufoeregrad,
+        ['getPerson(undefined)']: {
+          status: 'fulfilled',
+          endpointName: 'getPerson',
+          requestId: 'xTaE6mOydr5ZI75UXq4Wi',
+          startedTimeStamp: 1688046411971,
+          data: {
+            navn: 'Aprikos',
+            sivilstand: 'UGIFT',
+            foedselsdato,
+          },
+          fulfilledTimeStamp: 1688046412103,
+        },
+      }
+
+      it('Når hen er yngre enn 62 år, returneres null', async () => {
+        render(<GrunnlagAFP goToStart={vi.fn()} />, {
+          preloadedState: {
+            api: {
+              // @ts-ignore
+              queries: { ...mockedQueries },
+            },
+            userInput: {
+              ...userInputInitialState,
+              afp: 'ja_offentlig',
+            },
+          },
+        })
+
+        expect(screen.queryByText('grunnlag.afp.title')).toBeNull()
+      })
     })
 
-    it('Når hen har valgt AFP privat, viser riktig tittel med formatert inntekt og tekst', async () => {
-      render(<GrunnlagAFP goToStart={vi.fn()} />, {
-        preloadedState: {
-          api: {
-            // @ts-ignore
-            queries: { ...fulfilledGetLoependeVedtak75Ufoeregrad },
-          },
-          userInput: {
-            ...userInputInitialState,
-            afp: 'ja_privat',
-          },
-        },
+    describe('Gitt at brukeren er yngre enn minimum uttaksalderen,', () => {
+      const minAlderYearsBeforeNow = add(endOfDay(new Date()), {
+        years: -61,
+        months: -11,
       })
-      expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
-      expect(
-        screen.getByText('afp.privat (grunnlag.afp.ikke_beregnet)')
-      ).toBeVisible()
-      expect(
-        screen.getByText(
-          'Når du mottar uføretrygd, kan du ikke beregne AFP i kalkulatoren.',
-          { exact: false }
-        )
-      ).toBeInTheDocument()
-    })
+      const foedselsdato = format(minAlderYearsBeforeNow, DATE_BACKEND_FORMAT)
 
-    it('Når hen har valgt uten AFP, viser riktig tittel med formatert inntekt og tekst', async () => {
-      render(<GrunnlagAFP goToStart={vi.fn()} />, {
-        preloadedState: {
-          api: {
-            // @ts-ignore
-            queries: { ...fulfilledGetLoependeVedtak75Ufoeregrad },
+      const mockedQueries = {
+        ...fulfilledGetLoependeVedtak75Ufoeregrad,
+        ['getPerson(undefined)']: {
+          status: 'fulfilled',
+          endpointName: 'getPerson',
+          requestId: 'xTaE6mOydr5ZI75UXq4Wi',
+          startedTimeStamp: 1688046411971,
+          data: {
+            navn: 'Aprikos',
+            sivilstand: 'UGIFT',
+            foedselsdato,
           },
-          userInput: {
-            ...userInputInitialState,
-            afp: 'nei',
-          },
+          fulfilledTimeStamp: 1688046412103,
         },
-      })
-      expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
-      expect(screen.getByText('afp.nei')).toBeVisible()
-      expect(
-        screen.getByText('grunnlag.afp.ingress.nei.ufoeretrygd')
-      ).toBeInTheDocument()
-    })
+      }
 
-    it('Når hen har svart "vet ikke" på AFP, viser riktig tittel med formatert inntekt og tekst', async () => {
-      render(<GrunnlagAFP goToStart={vi.fn()} />, {
-        preloadedState: {
-          api: {
-            // @ts-ignore
-            queries: { ...fulfilledGetLoependeVedtak75Ufoeregrad },
+      it('Når hen har valgt AFP offentlig, viser riktig tittel med formatert inntekt og tekst', async () => {
+        render(<GrunnlagAFP goToStart={vi.fn()} />, {
+          preloadedState: {
+            api: {
+              // @ts-ignore
+              queries: { ...mockedQueries },
+            },
+            userInput: {
+              ...userInputInitialState,
+              afp: 'ja_offentlig',
+            },
           },
-          userInput: {
-            ...userInputInitialState,
-            afp: 'vet_ikke',
-          },
-        },
+        })
+
+        expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
+        expect(
+          screen.getByText('afp.offentlig (grunnlag.afp.ikke_beregnet)')
+        ).toBeVisible()
+        expect(
+          screen.getByText(
+            'Når du mottar uføretrygd, kan du ikke beregne AFP i kalkulatoren.',
+            { exact: false }
+          )
+        ).toBeInTheDocument()
       })
-      expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
-      expect(screen.getByText('afp.vet_ikke')).toBeVisible()
-      expect(
-        screen.getByText('grunnlag.afp.ingress.vet_ikke.ufoeretrygd')
-      ).toBeInTheDocument()
+
+      it('Når hen har valgt AFP privat, viser riktig tittel med formatert inntekt og tekst', async () => {
+        render(<GrunnlagAFP goToStart={vi.fn()} />, {
+          preloadedState: {
+            api: {
+              // @ts-ignore
+              queries: { ...mockedQueries },
+            },
+            userInput: {
+              ...userInputInitialState,
+              afp: 'ja_privat',
+            },
+          },
+        })
+        expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
+        expect(
+          screen.getByText('afp.privat (grunnlag.afp.ikke_beregnet)')
+        ).toBeVisible()
+        expect(
+          screen.getByText(
+            'Når du mottar uføretrygd, kan du ikke beregne AFP i kalkulatoren.',
+            { exact: false }
+          )
+        ).toBeInTheDocument()
+      })
+
+      it('Når hen har valgt uten AFP, viser riktig tittel med formatert inntekt og tekst', async () => {
+        render(<GrunnlagAFP goToStart={vi.fn()} />, {
+          preloadedState: {
+            api: {
+              // @ts-ignore
+              queries: { ...mockedQueries },
+            },
+            userInput: {
+              ...userInputInitialState,
+              afp: 'nei',
+            },
+          },
+        })
+        expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
+        expect(screen.getByText('afp.nei')).toBeVisible()
+        expect(
+          screen.getByText('grunnlag.afp.ingress.nei.ufoeretrygd')
+        ).toBeInTheDocument()
+      })
+
+      it('Når hen har svart "vet ikke" på AFP, viser riktig tittel med formatert inntekt og tekst', async () => {
+        render(<GrunnlagAFP goToStart={vi.fn()} />, {
+          preloadedState: {
+            api: {
+              // @ts-ignore
+              queries: { ...mockedQueries },
+            },
+            userInput: {
+              ...userInputInitialState,
+              afp: 'vet_ikke',
+            },
+          },
+        })
+        expect(screen.getByText('grunnlag.afp.title')).toBeVisible()
+        expect(screen.getByText('afp.vet_ikke')).toBeVisible()
+        expect(
+          screen.getByText('grunnlag.afp.ingress.vet_ikke.ufoeretrygd')
+        ).toBeInTheDocument()
+      })
     })
   })
 
