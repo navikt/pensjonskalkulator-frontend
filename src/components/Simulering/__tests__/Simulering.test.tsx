5bf986a5c6f060e67c4b17dc135a400e4d43e8ef
Marion Hauff <marion.hauff@nav.no>
2025-01-20 15:11:59 +0100

    justerer test

--- src/components/Simulering/__tests__/Simulering.test.tsx
+++ src/components/Simulering/__tests__/Simulering.test.tsx
@@ -131,7 +131,7 @@ describe('Simulering', () => {
         screen.getAllByText('100 000', {
           exact: false,
         })
-      ).toHaveLength(1)
+      ).toHaveLength(2)
       expect(
         screen.getByText('beregning.avansert.endring_banner.kr_md', {
           exact: false,
