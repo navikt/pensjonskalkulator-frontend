405db51a184102f2571ba6549ec0f2ce217385bd
Lucas Bateson <lucas.bateson@gmail.com>
2025-02-04 10:36:38 +0100

    Fjerne dette tar ikke kalkulatoren høyde for i Readmore

--- src/components/TidligstMuligUttaksalder/__tests__/TidligstMuligUttaksalder.test.tsx
+++ src/components/TidligstMuligUttaksalder/__tests__/TidligstMuligUttaksalder.test.tsx
@@ -46,7 +46,7 @@ describe('TidligstMuligUttaksalder', () => {
       ).not.toBeInTheDocument()
       expect(
         screen.getByText(
-          'Aldersgrensene vil øke gradvis fra 1964-kullet med én til to måneder per årskull,',
+          'Aldersgrensene vil øke gradvis fra 1964-kullet med én til to måneder per årskull.',
           {
             exact: false,
           }
@@ -85,7 +85,7 @@ describe('TidligstMuligUttaksalder', () => {
       ).toBeInTheDocument()
       expect(
         screen.getByText(
-          'Aldersgrensene vil øke gradvis fra 1964-kullet med én til to måneder per årskull,',
+          'Aldersgrensene vil øke gradvis fra 1964-kullet med én til to måneder per årskull.',
           {
             exact: false,
           }
