abed949d8703861aa321ad4b90adf8e178d3f7c7
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2025-02-24 09:10:24 +0100

    Refaktorer selectors (#1809)

--- src/pages/StepSamtykkePensjonsavtaler/StepSamtykkePensjonsavtaler.tsx
+++ src/pages/StepSamtykkePensjonsavtaler/StepSamtykkePensjonsavtaler.tsx
@@ -8,7 +8,6 @@ import { apiSlice } from '@/state/api/apiSlice'
 import { useAppDispatch, useAppSelector } from '@/state/hooks'
 import {
   selectSamtykke,
-  selectHarHentetOffentligTp,
   selectIsVeileder,
 } from '@/state/userInput/selectors'
 import { userInputActions } from '@/state/userInput/userInputReducer'
@@ -17,7 +16,6 @@ export function StepSamtykkePensjonsavtaler() {
   const intl = useIntl()
   const dispatch = useAppDispatch()
   const harSamtykket = useAppSelector(selectSamtykke)
-  const shouldFlush = useAppSelector(selectHarHentetOffentligTp)
   const isVeileder = useAppSelector(selectIsVeileder)
 
   const [{ onStegvisningNext, onStegvisningPrevious, onStegvisningCancel }] =
@@ -32,8 +30,8 @@ export function StepSamtykkePensjonsavtaler() {
   const onNext = (samtykkeData: BooleanRadio) => {
     const samtykke = samtykkeData === 'ja'
     dispatch(userInputActions.setSamtykke(samtykke))
-    if (shouldFlush && !samtykke) {
-      apiSlice.util.invalidateTags(['OffentligTp', 'Pensjonsavtaler'])
+    if (!samtykke) {
+      dispatch(apiSlice.util.invalidateTags(['OffentligTp', 'Pensjonsavtaler']))
     }
     if (onStegvisningNext) {
       onStegvisningNext()
