62368541423a2648f57ac3bf9429628131c4cbd0
9maPiltan <nima.piltan@nav.no>
2025-02-25 14:12:40 +0100

    Lagt til nye tester i StepAFP

--- src/pages/StepAFP/__tests__/StepAFP.test.tsx
+++ src/pages/StepAFP/__tests__/StepAFP.test.tsx
@@ -15,6 +15,8 @@ import { store } from '@/state/store'
 import { userInputInitialState } from '@/state/userInput/userInputReducer'
 import * as userInputReducerUtils from '@/state/userInput/userInputReducer'
 import { screen, render, userEvent, waitFor } from '@/test-utils'
+import * as alderUtils from '@/utils/alder'
+import * as loependeVedtakUtils from '@/utils/loependeVedtak'
 
 const initialGetState = store.getState
 
@@ -51,7 +53,7 @@ describe('StepAFP', () => {
     store.getState = initialGetState
   })
 
-  it('har riktig sidetittel og viser loader mens loaderen fetcher data', async () => {
+  it('har riktig sidetittel mens loaderen fetcher data', async () => {
     store.getState = vi.fn().mockImplementation(() => ({
       api: {
         queries: {
@@ -72,12 +74,9 @@ describe('StepAFP', () => {
     render(<RouterProvider router={router} />, {
       hasRouter: false,
     })
-    await waitFor(async () => {
-      expect(document.title).toBe('application.title.stegvisning.afp')
-    })
 
     await waitFor(async () => {
-      expect(await screen.findByTestId('afp-loader')).toBeVisible()
+      expect(document.title).toBe('application.title.stegvisning.afp')
     })
   })
 
@@ -95,7 +94,106 @@ describe('StepAFP', () => {
     expect(screen.getAllByRole('radio')).toHaveLength(4)
   })
 
-  it('Når brukeren velger afp og klikker på Neste, registrerer afp og navigerer videre til neste steg', async () => {
+  it('rendrer AFPPrivat når personen enten er født før 1963 og har vedtak om alderspensjon, eller når personen er født før 1963 og fylt 67 år', async () => {
+    vi.spyOn(alderUtils, 'isFoedtFoer1963').mockReturnValue(true)
+    vi.spyOn(alderUtils, 'isAlderOver67').mockReturnValue(true)
+    vi.spyOn(loependeVedtakUtils, 'isVedtakAlderspensjon').mockReturnValue(true)
+
+    const router = createMemoryRouter(routes, {
+      basename: BASE_PATH,
+      initialEntries: [`${BASE_PATH}${paths.afp}`],
+    })
+    render(<RouterProvider router={router} />, {
+      hasRouter: false,
+    })
+
+    await waitFor(() => {
+      expect(screen.getByTestId('afp-privat')).toBeInTheDocument()
+    })
+  })
+
+  it('rendrer AFPOvergangskullUtenAP når personen er født mellom 1954-1962 (overgangskull) og ikke har vedtak alderspensjon', async () => {
+    vi.spyOn(alderUtils, 'isFoedtFoer1963').mockReturnValue(false)
+    vi.spyOn(alderUtils, 'isOvergangskull').mockReturnValue(true)
+    vi.spyOn(loependeVedtakUtils, 'isVedtakAlderspensjon').mockReturnValue(
+      false
+    )
+
+    const router = createMemoryRouter(routes, {
+      basename: BASE_PATH,
+      initialEntries: [`${BASE_PATH}${paths.afp}`],
+    })
+    render(<RouterProvider router={router} />, {
+      hasRouter: false,
+    })
+
+    await waitFor(() => {
+      expect(screen.getByTestId('afp-overganskull')).toBeInTheDocument()
+    })
+  })
+
+  it('rendrer AFP når personen er født etter 1963 med og uten vedtak om alderspensjon', async () => {
+    vi.spyOn(alderUtils, 'isFoedtFoer1963').mockReturnValue(false)
+    vi.spyOn(alderUtils, 'isOvergangskull').mockReturnValue(false)
+
+    const router = createMemoryRouter(routes, {
+      basename: BASE_PATH,
+      initialEntries: [`${BASE_PATH}${paths.afp}`],
+    })
+    render(<RouterProvider router={router} />, {
+      hasRouter: false,
+    })
+
+    await waitFor(() => {
+      expect(screen.getByTestId('afp-etter-1963')).toBeInTheDocument()
+    })
+  })
+
+  it('Når brukeren som er i overgangskullet uten vedtak om alderspensjon velger afp og klikker på Neste, registrerer afp og skalBeregneAfp, og navigerer videre til neste steg', async () => {
+    vi.spyOn(alderUtils, 'isFoedtFoer1963').mockReturnValue(false)
+    vi.spyOn(alderUtils, 'isOvergangskull').mockReturnValue(true)
+    vi.spyOn(loependeVedtakUtils, 'isVedtakAlderspensjon').mockReturnValue(
+      false
+    )
+
+    const setAfpMock = vi.spyOn(
+      userInputReducerUtils.userInputActions,
+      'setAfp'
+    )
+    const setSkalBeregneAfpMock = vi.spyOn(
+      userInputReducerUtils.userInputActions,
+      'setSkalBeregneAfp'
+    )
+    const user = userEvent.setup()
+
+    const router = createMemoryRouter(routes, {
+      basename: BASE_PATH,
+      initialEntries: [`${BASE_PATH}${paths.afp}`],
+    })
+    render(<RouterProvider router={router} />, {
+      preloadedState: {
+        api: {
+          // @ts-ignore
+          queries: {
+            ...fulfilledGetLoependeVedtak0Ufoeregrad,
+          },
+        },
+      },
+      hasRouter: false,
+    })
+
+    const radioButtonsAfp = await screen.findAllByRole('radio')
+    await user.click(radioButtonsAfp[0])
+    const radioButtonsSkalBeregneAfp = await screen.findAllByRole('radio')
+    await user.click(radioButtonsSkalBeregneAfp[4])
+    await user.click(screen.getByText('stegvisning.neste'))
+
+    expect(setAfpMock).toHaveBeenCalledWith('ja_offentlig')
+    expect(setSkalBeregneAfpMock).toHaveBeenCalledWith(true)
+    expect(navigateMock).toHaveBeenCalledWith(paths.ufoeretrygdAFP)
+  })
+
+  it('Når brukeren født etter 1963 velger afp og klikker på Neste, registrerer afp og navigerer videre til neste steg', async () => {
     const setAfpMock = vi.spyOn(
       userInputReducerUtils.userInputActions,
       'setAfp'
