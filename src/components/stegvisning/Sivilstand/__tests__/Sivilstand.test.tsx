03b10f23c4c7e5c4c4440e919d5e02423608353b
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-02-25 09:08:46 +0100

    Flytt radio utils fra stegvisning til egen fil

--- src/components/stegvisning/Sivilstand/__tests__/Sivilstand.test.tsx
+++ src/components/stegvisning/Sivilstand/__tests__/Sivilstand.test.tsx
@@ -375,38 +375,4 @@ describe('stegvisning - Sivilstand', () => {
     )
     expect(screen.queryByText('stegvisning.avbryt')).not.toBeInTheDocument()
   })
-
-  describe('convertBooleanToBooleanRadio', () => {
-    it('convert true -> ja', () => {
-      const actual = convertBooleanToBooleanRadio(true)
-      expect(actual).toBe('ja')
-    })
-
-    it('convert false -> nei', () => {
-      const actual = convertBooleanToBooleanRadio(false)
-      expect(actual).toBe('nei')
-    })
-
-    it('convert null -> null', () => {
-      const actual = convertBooleanToBooleanRadio(null)
-      expect(actual).toBe(null)
-    })
-  })
-
-  describe('convertBooleanRadioToBoolean ', () => {
-    it('convert ja -> true', () => {
-      const actual = convertBooleanRadioToBoolean('ja')
-      expect(actual).toBe(true)
-    })
-
-    it('convert nei -> false', () => {
-      const actual = convertBooleanRadioToBoolean('nei')
-      expect(actual).toBe(false)
-    })
-
-    it('convert null -> null', () => {
-      const actual = convertBooleanRadioToBoolean(null)
-      expect(actual).toBe(null)
-    })
-  })
 })
