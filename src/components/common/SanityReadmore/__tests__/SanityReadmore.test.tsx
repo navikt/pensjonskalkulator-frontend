907de5578acc34a410dec49f03895032885709b4
Lucas Bateson <lucas.bateson@gmail.com>
2025-03-06 12:24:54 +0100

    Rettet opp tilbakemelding av tester

--- src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
+++ src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
@@ -5,17 +5,7 @@ import { SanityReadmore } from '@/components/common/SanityReadmore'
 import { mockErrorResponse } from '@/mocks/server'
 import { render, screen, waitFor } from '@/test-utils'
 
-vi.mock('@portabletext/react', () => ({
-  PortableText: vi.fn(() => (
-    <div data-testid="portable-text">Mocked PortableText</div>
-  )),
-}))
-
 describe('SanityReadmore', () => {
-  beforeEach(() => {
-    vi.clearAllMocks()
-  })
-
   describe('Gitt at Sanity er aktivert og innhold finnes', () => {
     it('rendrer ReadMore med korrekte props og innhold', async () => {
       render(
@@ -24,22 +14,15 @@ describe('SanityReadmore', () => {
         </SanityReadmore>
       )
 
-      await waitFor(() => {
-        const readMoreElement = screen.getByTestId(
-          'hva_er_opphold_utenfor_norge'
-        )
-        expect(readMoreElement).toBeInTheDocument()
-        expect(readMoreElement).toHaveAttribute(
-          'data-testid',
-          'hva_er_opphold_utenfor_norge'
-        )
-      })
+
+      const readMoreElement = await screen.findByTestId(
+        'hva_er_opphold_utenfor_norge'
+      )
+      expect(readMoreElement).toBeVisible()
 
       expect(
         screen.getByText('Hva som er opphold utenfor Norge')
-      ).toBeInTheDocument()
-
-      expect(PortableText).toHaveBeenCalled()
+      ).toBeVisible()
     })
   })
 
@@ -53,14 +36,13 @@ describe('SanityReadmore', () => {
         </SanityReadmore>
       )
 
-      await waitFor(() => {
-        expect(screen.getByText('Fallback innhold')).toBeInTheDocument()
-      })
+	  const readMoreElement = await screen.getByText('Fallback innhold')
+      expect(readMoreElement).toBeVisible()
+	  readMoreElement.click()
 
-      expect(
-        screen.queryByText('Hva som er opphold utenfor Norge')
-      ).not.toBeInTheDocument()
-      expect(PortableText).not.toHaveBeenCalled()
+	  expect(
+	    screen.queryByText('Hva som er opphold utenfor Norge')
+	  ).toBeNull()
     })
   })
 
@@ -72,14 +54,13 @@ describe('SanityReadmore', () => {
         </SanityReadmore>
       )
 
-      await waitFor(() => {
-        expect(screen.getByText('Fallback innhold')).toBeInTheDocument()
-      })
+      const readMoreElement = await screen.getByText('Fallback innhold')
+      expect(readMoreElement).toBeVisible()
+	  readMoreElement.click()
 
-      expect(
-        screen.queryByText('Hva som er opphold utenfor Norge')
-      ).not.toBeInTheDocument()
-      expect(PortableText).not.toHaveBeenCalled()
+	  expect(
+	    screen.queryByText('Hva som er opphold utenfor Norge')
+	  ).toBeNull()
     })
   })
 })
