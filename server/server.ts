69ae6f46971ab11f1d7d7d983935c9812e31f718
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-03-12 21:35:02 +0100

    Fjern eksport

--- server/server.ts
+++ server/server.ts
@@ -35,7 +35,7 @@ const env = ensureEnv({
   detaljertKalkulatorUrl: 'DETALJERT_KALKULATOR_URL',
 })
 
-export const isFoedtFoer1963 = (foedselsdato: string): boolean => {
+const isFoedtFoer1963 = (foedselsdato: string): boolean => {
   const LAST_DAY_1962 = new Date(1962, 11, 31)
   return (
     isBefore(new Date(foedselsdato), LAST_DAY_1962) ||
