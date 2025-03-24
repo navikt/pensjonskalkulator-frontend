f5451244578a55f88c19a004161fd28ee76ba83b
Halvor Haugan <halvor.haugan@nav.no>
2025-03-24 09:39:37 +0100

    fiks importrekkefølge og tester

--- src/components/common/SanityReadmore/SanityReadmore.tsx
+++ src/components/common/SanityReadmore/SanityReadmore.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useIntl } from 'react-intl'
-import clsx from 'clsx'
+
 import { PortableText } from '@portabletext/react'
+import clsx from 'clsx'
 
 import { ReadMore } from '@/components/common/ReadMore'
 import { SanityContext } from '@/context/SanityContext'
