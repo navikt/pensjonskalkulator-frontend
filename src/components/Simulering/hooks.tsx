7f6c6827a9611cfe2be75ddde3060146d7d8313d
9maPiltan <nima.piltan@nav.no>
2025-04-01 11:54:15 +0200

    test

--- src/components/Simulering/hooks.tsx
+++ src/components/Simulering/hooks.tsx
@@ -90,7 +90,7 @@ export const useSimuleringChartLocalState = (initialValues: {
   const pre2025OffentligAfpListe: AfpPrivatPensjonsberegning[] =
     pre2025OffentligAfp
       ? Array.from(
-          Array(Math.max(1, 67 - pre2025OffentligAfp.alderAar - 1)).keys(),
+          Array(Math.max(1, 67 - pre2025OffentligAfp.alderAar)).keys(),
           (_, index) => ({
             alder: pre2025OffentligAfp.alderAar + index,
             beloep:
