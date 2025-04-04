a8e9a3413d43c72fdafaa6a42b03649c19f6ee83
martinstorvoll <martin.storvoll@nav.no>
2025-04-04 23:43:21 +0200

    Fix for maanedlig utbetaling i mock

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -884,6 +884,8 @@ export interface components {
       sluttAlder?: components['schemas']['Alder']
       /** Format: int32 */
       aarligUtbetaling: number
+      /** Format: int32 */
+      maanedligUtbetaling?: number
     }
     PensjonsavtaleAlderSpecV2: {
       /** Format: int32 */
