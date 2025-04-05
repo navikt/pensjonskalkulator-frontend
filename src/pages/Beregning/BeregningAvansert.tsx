8aee4cf8b1e5450d07e3deedfdc873f34b4658d1
martinstorvoll <martin.storvoll@nav.no>
2025-04-05 05:19:10 +0200

    Lagt til månedsbeløp for gradert uttak

--- src/pages/Beregning/BeregningAvansert.tsx
+++ src/pages/Beregning/BeregningAvansert.tsx
@@ -327,6 +327,7 @@ export const BeregningAvansert = () => {
               afp={
                 maanedtligbeloep ? Math.round(maanedtligbeloep / 12) : undefined
               }
+              alderspensjon={alderspensjon}
             />
             {beregningsvalg === 'med_afp' && (
               <SanityGuidePanel
