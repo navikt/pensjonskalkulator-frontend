9496b765dafd921448a1a3e15ce1a2a1c4c09ccf
Lucas Bateson <lucas.bateson@gmail.com>
2025-04-07 17:13:13 +0200

    Fjere fallback mulighet fra selve SanityReadmore komponenten

--- src/components/common/SanityReadmore/SanityReadmore.tsx
+++ src/components/common/SanityReadmore/SanityReadmore.tsx
@@ -6,7 +6,6 @@ import clsx from 'clsx'
 
 import { ReadMore } from '@/components/common/ReadMore'
 import { SanityContext } from '@/context/SanityContext'
-import { useGetSanityFeatureToggleQuery } from '@/state/api/apiSlice'
 import { getSanityPortableTextComponents } from '@/utils/sanity'
 
 import styles from './SanityReadmore.module.scss'
@@ -14,18 +13,12 @@ import styles from './SanityReadmore.module.scss'
 interface Props {
   id: string
   className?: string
-  children?: React.ReactNode
 }
 
-export const SanityReadmore = ({ id, className, children }: Props) => {
+export const SanityReadmore = ({ id, className }: Props) => {
   const intl = useIntl()
   const { readMoreData } = React.useContext(SanityContext)
-  const { data: sanityFeatureToggle } = useGetSanityFeatureToggleQuery()
   const sanityContent = readMoreData[id]
-
-  if (children && (!sanityFeatureToggle?.enabled || !sanityContent)) {
-    return children
-  }
 
   return (
     <ReadMore
--- src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
+++ src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
@@ -6,25 +6,7 @@ import { render, screen } from '@/test-utils'
 
 describe('SanityReadmore', () => {
   describe('Gitt at Sanity er aktivert og innhold finnes', () => {
-    it('rendrer ReadMore med korrekte props og innhold når children finnes', async () => {
-      render(
-        <SanityReadmore id="hva_er_opphold_utenfor_norge">
-          <div>Child content</div>
-        </SanityReadmore>
-      )
-
-      const readMoreElement = await screen.findByTestId(
-        'hva_er_opphold_utenfor_norge'
-      )
-      expect(readMoreElement).toBeVisible()
-
-      expect(screen.getByText('Hva som er opphold utenfor Norge')).toBeVisible()
-
-      expect(screen.getByText('Lorem')).toBeInTheDocument()
-      expect(screen.queryByText('Child content')).not.toBeInTheDocument()
-    })
-
-    it('rendrer ReadMore med korrekte props og innhold når children ikke finnes', async () => {
+    it('rendrer ReadMore med korrekte props og innhold', async () => {
       render(<SanityReadmore id="hva_er_opphold_utenfor_norge" />)
 
       const readMoreElement = await screen.findByTestId(
@@ -33,25 +15,13 @@ describe('SanityReadmore', () => {
       expect(readMoreElement).toBeVisible()
 
       expect(screen.getByText('Hva som er opphold utenfor Norge')).toBeVisible()
+
       expect(screen.getByText('Lorem')).toBeInTheDocument()
     })
   })
 
   describe('Gitt at Sanity er deaktivert', () => {
-    it('viser fallback innhold og ikke Sanity innhold når children finnes', async () => {
-      mockErrorResponse('/feature/pensjonskalkulator.hent-tekster-fra-sanity')
-
-      render(
-        <SanityReadmore id="hva_er_opphold_utenfor_norge">
-          <p>Fallback innhold</p>
-        </SanityReadmore>
-      )
-
-      expect(screen.getByText('Fallback innhold')).toBeVisible()
-      expect(screen.queryByText('Hva som er opphold utenfor Norge')).toBeNull()
-    })
-
-    it('viser Sanity innhold når children ikke finnes, selv om feature toggle er deaktivert', async () => {
+    it('viser Sanity innhold selv om feature toggle er deaktivert', async () => {
       mockErrorResponse('/feature/pensjonskalkulator.hent-tekster-fra-sanity')
 
       render(<SanityReadmore id="hva_er_opphold_utenfor_norge" />)
@@ -67,18 +37,7 @@ describe('SanityReadmore', () => {
   })
 
   describe('Gitt at Sanity innhold ikke finnes', () => {
-    it('viser fallback innhold når id ikke finnes i readMoreData og children finnes', async () => {
-      render(
-        <SanityReadmore id="non-existent-id">
-          <p>Fallback innhold</p>
-        </SanityReadmore>
-      )
-
-      expect(screen.getByText('Fallback innhold')).toBeVisible()
-      expect(screen.queryByText('Hva som er opphold utenfor Norge')).toBeNull()
-    })
-
-    it('kaster runtime error når id ikke finnes i readMoreData og children ikke finnes', async () => {
+    it('kaster runtime error når id ikke finnes i readMoreData', async () => {
       expect(() => {
         render(<SanityReadmore id="non-existent-id" />)
       }).toThrow()
