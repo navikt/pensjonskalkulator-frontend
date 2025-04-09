a67a9126f2d88211225ca705551090d77e8d9e33
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-04-09 10:49:40 +0200

    Fjern unødvendig describes

--- src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
+++ src/components/common/SanityReadmore/__tests__/SanityReadmore.test.tsx
@@ -4,26 +4,22 @@ import { SanityReadmore } from '@/components/common/SanityReadmore'
 import { render, screen } from '@/test-utils'
 
 describe('SanityReadmore', () => {
-  describe('Gitt at Sanity er aktivert og innhold finnes', () => {
-    it('rendrer ReadMore med korrekte props og innhold', async () => {
-      render(<SanityReadmore id="hva_er_opphold_utenfor_norge" />)
+  it('rendrer ReadMore med korrekte props og innhold', async () => {
+    render(<SanityReadmore id="hva_er_opphold_utenfor_norge" />)
 
-      const readMoreElement = await screen.findByTestId(
-        'hva_er_opphold_utenfor_norge'
-      )
-      expect(readMoreElement).toBeVisible()
+    const readMoreElement = await screen.findByTestId(
+      'hva_er_opphold_utenfor_norge'
+    )
+    expect(readMoreElement).toBeVisible()
 
-      expect(screen.getByText('Hva som er opphold utenfor Norge')).toBeVisible()
+    expect(screen.getByText('Hva som er opphold utenfor Norge')).toBeVisible()
 
-      expect(screen.getByText('Lorem')).toBeInTheDocument()
-    })
+    expect(screen.getByText('Lorem')).toBeInTheDocument()
   })
 
-  describe('Gitt at Sanity innhold ikke finnes', () => {
-    it('kaster runtime error når id ikke finnes i readMoreData', async () => {
-      expect(() => {
-        render(<SanityReadmore id="non-existent-id" />)
-      }).toThrow()
-    })
+  it('kaster runtime error når id ikke finnes i readMoreData', async () => {
+    expect(() => {
+      render(<SanityReadmore id="non-existent-id" />)
+    }).toThrow()
   })
 })
