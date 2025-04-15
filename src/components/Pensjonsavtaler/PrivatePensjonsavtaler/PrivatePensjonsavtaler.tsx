d53e60f3673a0d1706a770fc71188d453946dc7e
9maPiltan <nima.piltan@nav.no>
2025-04-15 11:07:02 +0200

    alert margin lagt til

--- src/components/Pensjonsavtaler/PrivatePensjonsavtaler/PrivatePensjonsavtaler.tsx
+++ src/components/Pensjonsavtaler/PrivatePensjonsavtaler/PrivatePensjonsavtaler.tsx
@@ -44,7 +44,7 @@ export const PrivatePensjonsavtaler: React.FC<PrivatePensjonsavtalerProps> = ({
                 variant="info"
                 className={
                   isMobile
-                    ? styles['paragraph--mobile']
+                    ? styles.alert__margin_mobile
                     : styles['paragraph--desktop']
                 }
               >
@@ -67,7 +67,7 @@ export const PrivatePensjonsavtaler: React.FC<PrivatePensjonsavtalerProps> = ({
               variant="warning"
               className={
                 isMobile
-                  ? styles['paragraph--mobile']
+                  ? styles.alert__margin_mobile
                   : styles['paragraph--desktop']
               }
             >
