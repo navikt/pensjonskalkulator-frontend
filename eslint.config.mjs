a5d18fd8fc94ab62158322d7fcd7f05684bd598f
Christer Larsen <christer.larsen@nav.no>
2025-04-22 12:20:25 +0200

    PEK-1164: Add SonarLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslint from '@eslint/js'
 import importPlugin from 'eslint-plugin-import'
 import reactPlugin from 'eslint-plugin-react'
+import sonarjs from 'eslint-plugin-sonarjs'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -37,7 +38,8 @@ const defaultEslintConfig = tseslint.config(
     ignores: [...ignoredFiles],
   })),
   reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat['jsx-runtime']
+  reactPlugin.configs.flat['jsx-runtime'],
+  sonarjs.configs.recommended
 )
 
 export default [
@@ -81,6 +83,42 @@ export default [
       'import/export': 'error',
       'import/no-extraneous-dependencies': 'error',
       'import/no-duplicates': 'error',
+      'sonarjs/no-all-duplicated-branches': 'warn',
+      'sonarjs/no-element-overwrite': 'warn',
+      'sonarjs/no-empty-collection': 'warn',
+      'sonarjs/no-extra-arguments': 'warn',
+      'sonarjs/no-identical-conditions': 'warn',
+      'sonarjs/no-identical-expressions': 'warn',
+      'sonarjs/no-nested-functions': 'warn',
+      'sonarjs/no-nested-conditional': 'warn',
+      'sonarjs/no-ignored-return': 'warn',
+      'sonarjs/no-one-iteration-loop': 'warn',
+      'sonarjs/no-use-of-empty-return-value': 'warn',
+      'sonarjs/todo-tag': 'warn',
+      'sonarjs/class-name': 'warn',
+      'sonarjs/cognitive-complexity': 'warn',
+      'sonarjs/concise-regex': 'warn',
+      'sonarjs/max-switch-cases': 'warn',
+      'sonarjs/no-collapsible-if': 'warn',
+      'sonarjs/no-collection-size-mischeck': 'warn',
+      'sonarjs/no-duplicate-string': 'warn',
+      'sonarjs/no-duplicated-branches': 'warn',
+      'sonarjs/no-identical-functions': 'warn',
+      'sonarjs/no-inverted-boolean-check': 'warn',
+      'sonarjs/no-redundant-assignments': 'warn',
+      'sonarjs/no-redundant-boolean': 'warn',
+      'sonarjs/no-redundant-jump': 'warn',
+      'sonarjs/no-same-line-conditional': 'warn',
+      'sonarjs/no-small-switch': 'warn',
+      'sonarjs/no-unused-collection': 'warn',
+      'sonarjs/prefer-immediate-return': 'warn',
+      'sonarjs/prefer-object-literal': 'warn',
+      'sonarjs/prefer-single-boolean-return': 'warn',
+      'sonarjs/prefer-while': 'warn',
+      'sonarjs/pseudo-random': 'warn',
+      'sonarjs/regex-complexity': 'warn',
+      'sonarjs/slow-regex': 'warn',
+      'sonarjs/use-type-alias': 'warn',
     },
   },
 ]
