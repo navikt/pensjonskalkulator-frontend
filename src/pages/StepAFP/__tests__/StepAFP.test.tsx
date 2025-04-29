05c02474a8f11803bda24f71abe19953fee31a75
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-04-29 11:34:42 +0200

    Revert "Revert "PEK-1084: Forenkler navigasjon ved bruk av nettleserhistorikk (#2061)""
    This reverts commit 4f3d02cb8e2b70cf73faa296bc4ae68304253382.

--- src/pages/StepAFP/__tests__/StepAFP.test.tsx
+++ src/pages/StepAFP/__tests__/StepAFP.test.tsx
@@ -315,7 +315,7 @@ describe('StepAFP', () => {
     await waitFor(async () => {
       await user.click(await screen.findByText('stegvisning.tilbake'))
     })
-    expect(navigateMock).toHaveBeenCalledWith(paths.utenlandsopphold)
+    expect(navigateMock).toHaveBeenCalledWith(-1)
   })
 
   describe('Gitt at brukeren er logget på som veileder', async () => {
