e3ea094887c8054c57b62adcc12c3b2b156bd3cf
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2025-05-06 13:57:52 +0200

    PEK-1102: Fjern "0" fra Grunnlag: Alderspensjon (NAV) (#1914)
    Co-authored-by: martinstorvoll <martin.storvoll@nav.no>

--- src/pages/Beregning/BeregningAvansert.tsx
+++ src/pages/Beregning/BeregningAvansert.tsx
@@ -345,6 +345,7 @@ export const BeregningAvansert = () => {
                       .pensjonBeholdningFoerUttakBeloep
                   : undefined
               }
+              isEndring={isEndring}
             />
           </>
         )}
