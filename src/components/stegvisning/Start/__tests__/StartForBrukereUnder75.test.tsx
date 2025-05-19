79f15f76c643afa9b65f57408920bca29d3ac8ad
eshaswinimk <eshaswini.murali.krishna@nav.no>
2025-05-20 01:38:23 +0200

    Add unit test for pre2025_offentlig_afp start ingress

--- src/components/stegvisning/Start/__tests__/StartForBrukereUnder75.test.tsx
+++ src/components/stegvisning/Start/__tests__/StartForBrukereUnder75.test.tsx
@@ -235,6 +235,32 @@ describe('stegvisning - Start', () => {
     ).not.toBeInTheDocument()
   })
 
+  it('rendrer slik den skal med vedtak om pre2025 offentlig Afp', async () => {
+    render(
+      <StartForBrukereUnder75
+        navn="Ola"
+        loependeVedtak={{
+          ufoeretrygd: {
+            grad: 0,
+          },
+          pre2025OffentligAfp: {
+            fom: '2024-08-01',
+          },
+        }}
+        onCancel={onCancelMock}
+        onNext={onNextMock}
+      />
+    )
+
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'stegvisning.start.title Ola!'
+    )
+    expect(screen.getByText('Du har nå ', { exact: false })).toBeVisible()
+    expect(
+      screen.getByText('AFP i offentlig sektor', { exact: false })
+    ).toBeVisible()
+  })
+
   it('kaller onNext når brukeren klikker på Neste', async () => {
     const user = userEvent.setup()
     render(
