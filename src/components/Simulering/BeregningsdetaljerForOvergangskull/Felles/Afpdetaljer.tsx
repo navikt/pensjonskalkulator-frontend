dbd33cf853e5b08c304726801a4d90f598a7c575
9maPiltan <nima.piltan@nav.no>
2025-06-17 09:59:01 +0200

    Dynamisk aldervisning i afpdetaljer

--- src/components/Simulering/BeregningsdetaljerForOvergangskull/Felles/Afpdetaljer.tsx
+++ src/components/Simulering/BeregningsdetaljerForOvergangskull/Felles/Afpdetaljer.tsx
@@ -91,7 +91,7 @@ export const Afpdetaljer: React.FC<AfpdetaljerProps> = ({
                 id="beregning.detaljer.afpPrivat.heltUttak.title"
                 values={{
                   ...getFormatMessageValues(),
-                  alderAar: `67 år`,
+                  alderAar: `${uttaksalder?.aar} år`,
                   alderMd: '',
                   grad: 100,
                 }}
