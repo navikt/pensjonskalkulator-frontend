dbb50d146214dc56df987e2b0e687980196057c4
9maPiltan <nima.piltan@nav.no>
2025-06-18 11:01:28 +0200

    Refaktorering

--- src/components/Simulering/BeregningsdetaljerForOvergangskull/BeregningsdetaljerForOvergangskull.tsx
+++ src/components/Simulering/BeregningsdetaljerForOvergangskull/BeregningsdetaljerForOvergangskull.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { Box } from '@navikt/ds-react'
+import { Box, HStack, VStack } from '@navikt/ds-react'
 
-import { BeregningsdetaljerDesktop } from './BeregningsdetaljerDesktop/BeregningsdetaljerDesktop'
-import { BeregningsdetaljerMobil } from './BeregningsdetaljerMobil/BeregningsdetaljerMobil'
+import { AfpDetaljer } from './Felles/AfpDetaljer'
+import { AlderspensjonDetaljer } from './Felles/AlderspensjonDetaljer'
+import { OpptjeningDetaljer } from './Felles/OpptjeningDetaljer'
 import { useBeregningsdetaljer } from './hooks'
 
 import styles from './BeregningsdetaljerForOvergangskull.module.scss'
@@ -31,30 +32,37 @@ export const BeregningsdetaljerForOvergangskull: React.FC<Props> = ({
     pre2025OffentligAfp
   )
 
+  const renderDetaljer = () => (
+    <>
+      <AlderspensjonDetaljer
+        alderspensjonDetaljerListe={alderspensjonDetaljerListe}
+        hasPre2025OffentligAfpUttaksalder={Boolean(
+          opptjeningPre2025OffentligAfpListe?.length
+        )}
+      />
+      <OpptjeningDetaljer
+        opptjeningKap19Liste={opptjeningKap19Liste}
+        opptjeningKap20Liste={opptjeningKap20Liste}
+      />
+      <AfpDetaljer
+        opptjeningAfpPrivatListe={opptjeningAfpPrivatListe}
+        opptjeningPre2025OffentligAfpListe={opptjeningPre2025OffentligAfpListe}
+      />
+    </>
+  )
+
   return (
     <Box marginBlock="10 0" data-testid="beregningsdetaljer-for-overgangskull">
       <div className={styles.beregningsdetaljerForOvergangskullDesktopOnly}>
-        <BeregningsdetaljerDesktop
-          alderspensjonDetaljerListe={alderspensjonDetaljerListe}
-          opptjeningKap19Liste={opptjeningKap19Liste}
-          opptjeningKap20Liste={opptjeningKap20Liste}
-          opptjeningAfpPrivatListe={opptjeningAfpPrivatListe}
-          opptjeningPre2025OffentligAfpListe={
-            opptjeningPre2025OffentligAfpListe
-          }
-        />
+        <HStack gap="20" width="100%">
+          {renderDetaljer()}
+        </HStack>
       </div>
 
       <div className={styles.beregningsdetaljerForOvergangskullMobileOnly}>
-        <BeregningsdetaljerMobil
-          alderspensjonDetaljerListe={alderspensjonDetaljerListe}
-          opptjeningKap19Liste={opptjeningKap19Liste}
-          opptjeningKap20Liste={opptjeningKap20Liste}
-          opptjeningAfpPrivatListe={opptjeningAfpPrivatListe}
-          opptjeningPre2025OffentligAfpListe={
-            opptjeningPre2025OffentligAfpListe
-          }
-        />
+        <VStack gap="4 8" width="100%" marginBlock="2 0">
+          {renderDetaljer()}
+        </VStack>
       </div>
     </Box>
   )
