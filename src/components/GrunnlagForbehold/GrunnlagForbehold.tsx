d3bfd3c419af8f0cf48dbe9d72caa79ffbfb89d0
EMK <eshaswini.murali.krishna@nav.no>
2025-08-20 12:38:59 +0200

    PEK 852 log links (#2337)

--- src/components/GrunnlagForbehold/GrunnlagForbehold.tsx
+++ src/components/GrunnlagForbehold/GrunnlagForbehold.tsx
@@ -1,10 +1,10 @@
 import { FormattedMessage, useIntl } from 'react-intl'
-import { Link as ReactRouterLink } from 'react-router'
 
 import { ExternalLinkIcon } from '@navikt/aksel-icons'
 import { BodyLong, Heading, HeadingProps, Link } from '@navikt/ds-react'
 
 import { paths } from '@/router/constants'
+import { logger } from '@/utils/logging'
 
 import styles from './GrunnlagForbehold.module.scss'
 
@@ -24,10 +24,15 @@ export const GrunnlagForbehold = ({ headingLevel }: Props) => {
       <BodyLong className={styles.text}>
         <FormattedMessage id="grunnlag.forbehold.ingress_1" />
         <Link
-          as={ReactRouterLink}
-          to={paths.forbehold}
+          href={`/pensjon/kalkulator${paths.forbehold}`}
           target="_blank"
           inlineText
+          onClick={() => {
+            logger('link åpnet', {
+              href: `/pensjon/kalkulator${paths.forbehold}`,
+              target: '_blank',
+            })
+          }}
         >
           <FormattedMessage id="grunnlag.forbehold.link" />
           <ExternalLinkIcon
