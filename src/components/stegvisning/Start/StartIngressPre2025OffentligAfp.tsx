caa6fb10bd114ce9e0350ab34852e9c464249c2e
eshaswinimk <eshaswini.murali.krishna@nav.no>
2025-08-28 10:35:48 +0200

    Change ingress text as per updated design spec

--- src/components/stegvisning/Start/StartIngressPre2025OffentligAfp.tsx
+++ src/components/stegvisning/Start/StartIngressPre2025OffentligAfp.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl'
 
 import { BodyLong } from '@navikt/ds-react'
 
+import { TelefonLink } from '@/components/common/TelefonLink'
 import { DATE_ENDUSER_FORMAT } from '@/utils/dates'
 import { getFormatMessageValues } from '@/utils/translations'
 
@@ -15,31 +16,37 @@ export const StartIngressPre2025OffentligAfp: React.FC<
   StartIngressPre2025OffentligAfpProps
 > = ({ loependeVedtak }) => {
   const fremtidigAlderspensjon = loependeVedtak.fremtidigAlderspensjon
+  if (loependeVedtak.alderspensjon?.grad === 0 && fremtidigAlderspensjon) {
+    return (
+      <div>
+        <span data-testid="stegvisning-start-ingress-pre2025-offentlig-afp">
+          <FormattedMessage
+            id="stegvisning.start.endring.ingress.pre2025_offentlig_afp_fremtidig"
+            values={{
+              ...getFormatMessageValues(),
+              grad: fremtidigAlderspensjon.grad,
+              fom: format(
+                parseISO(fremtidigAlderspensjon.fom),
+                DATE_ENDUSER_FORMAT
+              ),
+              link: <TelefonLink />,
+            }}
+          />
+        </span>
+      </div>
+    )
+  }
   return (
     <div>
       <span data-testid="stegvisning-start-ingress-pre2025-offentlig-afp">
         {loependeVedtak.alderspensjon?.grad === 0 ? (
-          fremtidigAlderspensjon ? (
-            <FormattedMessage
-              id="stegvisning.start.endring.ingress.pre2025_offentlig_afp_fremtidig"
-              values={{
-                ...getFormatMessageValues(),
-                grad: fremtidigAlderspensjon.grad,
-                fom: format(
-                  parseISO(fremtidigAlderspensjon.fom),
-                  DATE_ENDUSER_FORMAT
-                ),
-              }}
-            />
-          ) : (
-            <FormattedMessage
-              id="stegvisning.start.endring.ingress.pre2025_offentlig_afp"
-              values={{
-                ...getFormatMessageValues(),
-                grad: loependeVedtak?.alderspensjon?.grad,
-              }}
-            />
-          )
+          <FormattedMessage
+            id="stegvisning.start.endring.ingress.pre2025_offentlig_afp"
+            values={{
+              ...getFormatMessageValues(),
+              grad: loependeVedtak?.alderspensjon?.grad,
+            }}
+          />
         ) : (
           <FormattedMessage
             id="stegvisning.start.ingress.pre2025_offentlig_afp"
