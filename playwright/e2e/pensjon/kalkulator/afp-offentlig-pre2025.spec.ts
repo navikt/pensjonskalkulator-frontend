f6c0e0f8838157028d54c5d6f31bd7b62e2499bc
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-10-22 14:24:02 +0200

    Implementer person mock utility med mulighet for dynamiske aldre

--- playwright/e2e/pensjon/kalkulator/afp-offentlig-pre2025.spec.ts
+++ playwright/e2e/pensjon/kalkulator/afp-offentlig-pre2025.spec.ts
@@ -1,31 +1,23 @@
 import { expect, test } from '../../../base'
 import { loadJSONMock } from '../../../utils/mock'
+import { person } from '../../../utils/mocks'
 import { fillOutStegvisning } from '../../../utils/navigation'
 
-type PersonMock = { foedselsdato?: string } & Record<string, unknown>
 type LoependeVedtakMock = {
   pre2025OffentligAfp?: { fom: string }
 } & Record<string, unknown>
 
-// Disable auto-auth for this test file since we need custom mocks
 test.use({ autoAuth: false })
 
 test.beforeEach(async ({ page }) => {
-  const person = (await loadJSONMock('person.json')) as PersonMock
-  person.foedselsdato = '1960-01-01'
-
   const loependeVedtak = (await loadJSONMock(
     'loepende-vedtak.json'
   )) as LoependeVedtakMock
   loependeVedtak.pre2025OffentligAfp = { fom: '2024-08-01' }
 
-  // Manually set up mocks and auth
   const { authenticate } = await import('../../../utils/auth')
   await authenticate(page, [
-    {
-      url: /\/pensjon\/kalkulator\/api\/v5\/person/,
-      jsonResponse: person,
-    },
+    await person({ foedselsdato: '1960-01-01' }),
     {
       url: /\/pensjon\/kalkulator\/api\/v4\/vedtak\/loepende-vedtak/,
       jsonResponse: loependeVedtak,
