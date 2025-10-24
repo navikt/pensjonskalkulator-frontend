18dc5dda9d0ebd72dd6313fd15823f97df4b438f
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-10-24 09:33:32 +0200

    Oppdater spraakvelger test

--- playwright/e2e/pensjon/kalkulator/spraakvelger.spec.ts
+++ playwright/e2e/pensjon/kalkulator/spraakvelger.spec.ts
@@ -1,15 +1,15 @@
 import { test } from '../../../base'
-import { clickButton, expectElementVisible } from '../../../utils/form'
+import { expectElementVisible } from '../../../utils/form'
 
 test.describe('Språkvelger', () => {
   test('setter Norsk Bokmål som default språk i løsningen for innhold styrt av react-intl og Sanity', async ({
     page,
   }) => {
     await expectElementVisible(page, 'stegvisning-start-button')
-    await clickButton(page, 'stegvisning-start-button')
+    await page.getByTestId('stegvisning-start-button').click()
 
     await expectElementVisible(page, 'sivilstand-heading')
-    await clickButton(page, 'stegvisning-neste-button')
+    await page.getByTestId('stegvisning-neste-button').click()
 
     await expectElementVisible(page, 'utenlandsopphold-heading')
     await expectElementVisible(page, 'hva_er_opphold_utenfor_norge')
