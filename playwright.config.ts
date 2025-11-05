16c6f29d971adecfb734b37a47d8429a489e4755
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-11-05 11:05:39 +0100

    Legg til browsers (Firefox og WebKit) i playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,6 +66,20 @@ export default defineConfig({
         },
       },
     },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        headless: true,
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        headless: true,
+      },
+    },
   ],
 
   webServer: {
