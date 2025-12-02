a27824c992d09da059e9253c1ba11f8674381b34
Krishna <Eshaswini.Murali.Krishna@MacBookPro.home>
2025-12-02 17:03:56 +0100

    Show table with rows only

--- src/components/Simulering/pdf-utils.ts
+++ src/components/Simulering/pdf-utils.ts
@@ -164,8 +164,8 @@ function getAfpTable(afpSection: AfpSectionConfig): string {
     })
     .join('\n')
 
-  return `<div style='margin:0 0 12px 0; width: 33%'>
-        <h4 class="afp-grunnlag-title">${titleId}</h4>
+  return `<div style='margin:0; width: 33%;'>
+        ${titleId ? `<h4 class="afp-grunnlag-title">${titleId}</h4>` : ''}
       <table role='presentation'>${rows}</table>
     </div>`
 }
@@ -189,15 +189,37 @@ export function getAldersPensjonDetaljerHtmlTable(
       })
       .join('\n')
 
-  const tableA = `<table class="pdf-table-type2"><thead><tr><th colspan="2" style='text-align: left;'>Månedlig alderspensjon (Nav)</th></tr></thead><tbody>${getTableRows(alderspensjon)}</tbody></table>`
-  const tableB = `<table><thead><tr><th colspan="2" style='text-align: left;'>Opptjening etter gamle regler</th></tr></thead><tbody>${getTableRows(opptjeningKap19)}</tbody></table>`
-  const tableC = `<table><thead><tr><th colspan="2" style='text-align: left;'>Opptjening etter nye regler</th></tr></thead><tbody>${getTableRows(opptjeningKap20)}</tbody></table>`
+  const tableARows = getTableRows(alderspensjon)
+  const tableBRows = getTableRows(opptjeningKap19)
+  const tableCRows = getTableRows(opptjeningKap20)
+
+  const tableA = tableARows
+    ? `<table class="pdf-table-type2"><thead><tr><th colspan="2" style='text-align: left;'>Månedlig alderspensjon (Nav)</th></tr></thead><tbody>${tableARows}</tbody></table>`
+    : null
+
+  const tableB = tableBRows
+    ? `<table><thead><tr><th colspan="2" style='text-align: left;'>Opptjening etter gamle regler</th></tr></thead><tbody>${tableBRows}</tbody></table>`
+    : null
+
+  const tableC = tableCRows
+    ? `<table><thead><tr><th colspan="2" style='text-align: left;'>Opptjening etter nye regler</th></tr></thead><tbody>${tableCRows}</tbody></table>`
+    : null
+
+  // If none of the tables have rows, return an empty string
+  if (!tableA && !tableB && !tableC) return ''
+
+  // Build an array of non-empty table cells so we only render columns that exist
+  const cells: string[] = []
+  if (tableA) cells.push(`<td class="pdf-td-type2">${tableA}</td>`)
+  if (tableB)
+    cells.push(
+      `<td class="pdf-td-type2" style='padding-left: 8px;'>${tableB}</td>`
+    )
+  if (tableC) cells.push(`<td class="pdf-td-type2">${tableC}</td>`)
 
   return `<table role='presentation' style='width:100%;'>
       <tr class="pdf-table-wrapper-row">
-        <td class="pdf-td-type2">${tableA}</td>
-        <td class="pdf-td-type2" style='padding-left: 8px;'>${tableB}</td>
-        <td class="pdf-td-type2">${tableC}</td>
+        ${cells.join('\n')}
       </tr>
     </table>`
 }
