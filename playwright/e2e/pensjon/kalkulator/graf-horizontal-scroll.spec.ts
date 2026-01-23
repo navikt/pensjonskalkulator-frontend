58d4b0326b8ae4a6e960bbe26d18b24a70f5812a
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2026-01-23 10:19:00 +0100

    style: fix prettier formatting

--- playwright/e2e/pensjon/kalkulator/graf-horizontal-scroll.spec.ts
+++ playwright/e2e/pensjon/kalkulator/graf-horizontal-scroll.spec.ts
@@ -1,7 +1,8 @@
+import { fillOutStegvisning } from 'utils/navigation'
+
 import { expect, test } from '../../../base'
 import { authenticate } from '../../../utils/auth'
 import { person, tidligsteUttaksalder } from '../../../utils/mocks'
-import { fillOutStegvisning } from 'utils/navigation'
 
 test.describe('Graf Horizontal scroll', () => {
   test.describe('Gitt at grafen rendres på desktop,', () => {
