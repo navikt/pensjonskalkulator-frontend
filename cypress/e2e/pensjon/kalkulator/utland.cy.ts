420d2dfeb745393799350deeb3db0ed967e4fb4a
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2026-01-28 14:15:26 +0100

    Oppdater land liste expect

--- cypress/e2e/pensjon/kalkulator/utland.cy.ts
+++ cypress/e2e/pensjon/kalkulator/utland.cy.ts
@@ -53,9 +53,9 @@ describe('Utland', () => {
           cy.get('[data-testid="utenlandsopphold-land"]').then(
             (selectElements) => {
               const options = selectElements.find('option')
-              expect(options.length).equal(257)
+              expect(options.length).equal(255)
               expect(options.eq(1).text()).equal('Afghanistan')
-              expect(options.eq(256).text()).equal('ukjent')
+              expect(options.eq(254).text()).equal('ukjent')
             }
           )
         })
