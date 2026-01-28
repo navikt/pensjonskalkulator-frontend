faa24da0ce282d0a0464a4363d3fdbeb8cdc9535
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2026-01-28 15:26:53 +0100

    Remove Playwright reprorting

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,9 +15,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers,
-  reporter: process.env.CI
-    ? 'line'
-    : [['html', { outputFolder: 'playwright/report', open: 'never' }]],
   timeout: 30000,
   expect: {
     timeout: 5000,
